d1cd1d4c8fe2589224ddcbb83f629cfe0ac97325
Mike McLaughlin <mikem@microsoft.com>
2018-05-09 12:42:25 -0700

    Add license headers
    Add .NET Core install script that works on Fedora, OpenSuse, etc.
    
    Update to dotnet 2.1.300-rc1-008673

--- eng/cibuild.sh
+++ eng/cibuild.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Copyright (c) .NET Foundation and contributors. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 source="${BASH_SOURCE[0]}"
 
--- src/SOS/tests/testsos.sh
+++ src/SOS/tests/testsos.sh
@@ -13,8 +13,13 @@ fi
 __Plugin=$__NativeBinDir/libsosplugin.so
 __Host=$__ProjectRoot/.dotnet/dotnet
 __TestProgram=$__ManagedBinDir/TestDebuggee/netcoreapp2.0/TestDebuggee.dll
+__LLDB_Path=$LLDB_PATH
+
+if [[ "$__LLDB_Path" = "" ]]; then
+    __LLDB_Path=lldb
+fi
 
 cd $__ProjectRoot/src/SOS/tests/
-rm StressLog.txt
-python2 $__ProjectRoot/src/SOS/tests/test_libsosplugin.py --lldb $LLDB_PATH --host $__Host --plugin $__Plugin --logfiledir $__LogFileDir --assembly $__TestProgram
+rm -f StressLog.txt
+python2 $__ProjectRoot/src/SOS/tests/test_libsosplugin.py --lldb $__LLDB_Path --host $__Host --plugin $__Plugin --logfiledir $__LogFileDir --assembly $__TestProgram
 
