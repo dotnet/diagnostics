6e1b71ff99001dd4a09fd9896af2160e9196ee18
noahfalk <noahfalk@microsoft.com>
2019-02-04 23:56:30 -0800

    Small engineering and test updates
    1) Update to 2.1.503 SDK/2.1.7 runtime.
    2) Test against 2.2 instead of 2.0
    3) Push test logs on public ci
    
    (work originally mikem, I just split his mega-commit so history would be a little more understandable)

--- eng/install-test-runtimes.ps1
+++ eng/install-test-runtimes.ps1
@@ -12,16 +12,16 @@ param(
 Set-StrictMode -Version Latest
 $ErrorActionPreference="Stop"
 
-$RuntimeVersion11="1.1.9"
-$RuntimeVersion20="2.0.9"
+$RuntimeVersion11="1.1.10"
+$RuntimeVersion22="2.2.1"
 $DailyTestText="true"
 
-# Install the other versions of .NET Core runtime we are going to test. 1.1.x, 2.0.x, 2.1.x (installed with the CLI)
+# Install the other versions of .NET Core runtime we are going to test. 1.1.x, 2.1.x (installed with the CLI), 2.2.x
 # and latest. Only install the latest master for daily jobs and leave the RuntimeVersion* config properties blank.
 if (!$DailyTest) {
     $DailyTestText="false"
     . $DotNetDir\dotnet-install.ps1 -Version $RuntimeVersion11 -Architecture $BuildArch -SkipNonVersionedFiles -Runtime dotnet -InstallDir $DotNetDir
-    . $DotNetDir\dotnet-install.ps1 -Version $RuntimeVersion20 -Architecture $BuildArch -SkipNonVersionedFiles -Runtime dotnet -InstallDir $DotNetDir
+    . $DotNetDir\dotnet-install.ps1 -Version $RuntimeVersion22 -Architecture $BuildArch -SkipNonVersionedFiles -Runtime dotnet -InstallDir $DotNetDir
 }
 
 . $DotNetDir\dotnet-install.ps1 -Channel $Branch -Version latest -Architecture $BuildArch -SkipNonVersionedFiles -Runtime dotnet -InstallDir $DotNetDir
@@ -43,8 +43,8 @@ if (Test-Path $VersionFile) {
     '<Configuration>
 <DailyTest>' + $DailyTestText  +'</DailyTest>
 <RuntimeVersion11>' + $RuntimeVersion11 + '</RuntimeVersion11>
-<RuntimeVersion20>' + $RuntimeVersion20 + '</RuntimeVersion20>
 <RuntimeVersion21>' + $RuntimeVersion21 + '</RuntimeVersion21>
+<RuntimeVersion22>' + $RuntimeVersion22 + '</RuntimeVersion22>
 <RuntimeVersionLatest>' + $RuntimeVersionLatest + '</RuntimeVersionLatest>
 </Configuration>' | Set-Content $ConfigFile
 
