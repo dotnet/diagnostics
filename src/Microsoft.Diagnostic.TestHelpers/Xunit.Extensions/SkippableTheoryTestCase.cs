d5700ef3209cd30b982b37bfc800c12e78455c64
Juan Hoyos <juan.hoyos@microsoft.com>
2019-05-03 18:39:46 -0700

    Update xunit versions

--- src/Microsoft.Diagnostic.TestHelpers/Xunit.Extensions/SkippableTheoryTestCase.cs
+++ src/Microsoft.Diagnostic.TestHelpers/Xunit.Extensions/SkippableTheoryTestCase.cs
@@ -11,8 +11,8 @@ namespace Xunit.Extensions
         [Obsolete("Called by the de-serializer; should only be called by deriving classes for de-serialization purposes")]
         public SkippableTheoryTestCase() { }
 
-        public SkippableTheoryTestCase(IMessageSink diagnosticMessageSink, Xunit.Sdk.TestMethodDisplay defaultMethodDisplay, ITestMethod testMethod)
-            : base(diagnosticMessageSink, defaultMethodDisplay, testMethod) { }
+        public SkippableTheoryTestCase(IMessageSink diagnosticMessageSink, TestMethodDisplay defaultMethodDisplay, TestMethodDisplayOptions defaultMethodDisplayOptions, ITestMethod testMethod)
+            : base(diagnosticMessageSink, defaultMethodDisplay, defaultMethodDisplayOptions, testMethod) { }
 
         public override async Task<RunSummary> RunAsync(IMessageSink diagnosticMessageSink,
                                                         IMessageBus messageBus,
