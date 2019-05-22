05e08e55a743bc5c3dfa631299c71058d0124d16
John Salem <josalem@microsoft.com>
2019-05-22 13:22:01 -0700

    * bring error codes in line with the now checked0in spec

--- src/Microsoft.Diagnostics.Tools.RuntimeClient/DiagnosticsIpc/IpcErrorCode.cs
+++ src/Microsoft.Diagnostics.Tools.RuntimeClient/DiagnosticsIpc/IpcErrorCode.cs
@@ -6,13 +6,15 @@ namespace Microsoft.Diagnostics.Tools.RuntimeClient.DiagnosticsIpc
 {
     internal enum DiagnosticServerErrorCode : uint
     {
-        OK = 0x00000000,
-        BadEncoding = 0x00000001,
-        UnknownCommandSet = 0x00000002,
-        UnknownCommandId = 0x00000003,
-        UnknownVersion = 0x00000004,
+        OK              = 0x00000000,
+        BadEncoding     = 0x00000001,
+        UnknownCommand  = 0x00000002,
+        UnknownMagic    = 0x00000003,
+        BadInput        = 0x00000004,
+
+        ServerError     = 0x00000005,
         // future
 
-        BAD = 0xFFFFFFFF,
+        BAD             = 0xFFFFFFFF,
     }
 }
