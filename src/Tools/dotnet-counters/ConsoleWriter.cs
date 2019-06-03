dotnet/diagnostics

commit 9b94de40bd1b73304bb16bc868829cb1ef6bea80

Display unknown EventSource's provider name properly (#299)

src/Tools/dotnet-counters/ConsoleWriter.cs
@@ -16,11 +16,13 @@ namespace Microsoft.Diagnostics.Tools.Counters
         private int maxRow;  // Running maximum of row number
         private int maxCol;  // Running maximum of col number
         private Dictionary<string, int> knownProvidersRowNum;
+        private Dictionary<string, int> unknownProvidersRowNum;
 
         public ConsoleWriter()
         {
             displayPosition = new Dictionary<string, (int, int)>();
             knownProvidersRowNum = new Dictionary<string, int>();
+            unknownProvidersRowNum = new Dictionary<string, int>();
 
             foreach(CounterProvider provider in KnownData.GetAllProviders())
             {
@@ -64,7 +66,7 @@ namespace Microsoft.Diagnostics.Tools.Counters
                     {
                         knownProvidersRowNum[providerName] = maxRow + 1;
                         Console.SetCursorPosition(0, maxRow);
-                        Console.WriteLine(providerName);
+                        Console.WriteLine($"[{providerName}]");
                         maxRow += 1;
                     }
 
@@ -84,6 +86,14 @@ namespace Microsoft.Diagnostics.Tools.Counters
                 else
                 {
                     // If it's from an unknown provider, just append it at the end.
+                    if (!unknownProvidersRowNum.ContainsKey(providerName))
+                    {
+                        knownProvidersRowNum[providerName] = maxRow + 1;
+                        Console.SetCursorPosition(0, maxRow);
+                        Console.WriteLine($"[{providerName}]");
+                        maxRow += 1;
+                    }
+
                     string displayName = payload.GetDisplay();
                     if (string.IsNullOrEmpty(displayName))
                     {

src/Tools/dotnet-counters/CounterMonitor.cs
@@ -101,11 +101,6 @@ namespace Microsoft.Diagnostics.Tools.Counters
                 return 1;
             }
 
-            if (_interval == 0) {
-                _console.Error.WriteLine("refreshInterval is required.");
-                return 1;
-            }
-
             String providerString;
 
             if (_counterList.Count == 0)

commit 9f6295e34b3361db9353a03762dd29f9f4fa4884

Fix regression introduced by PR #299

src/Tools/dotnet-counters/ConsoleWriter.cs
@@ -88,7 +88,7 @@ namespace Microsoft.Diagnostics.Tools.Counters
                     // If it's from an unknown provider, just append it at the end.
                     if (!unknownProvidersRowNum.ContainsKey(providerName))
                     {
-                        knownProvidersRowNum[providerName] = maxRow + 1;
+                        unknownProvidersRowNum[providerName] = maxRow + 1;
                         Console.SetCursorPosition(0, maxRow);
                         Console.WriteLine($"[{providerName}]");
                         maxRow += 1;