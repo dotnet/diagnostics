623ae4fa7d7ca4fe2e6fc3cca44947e3e10c0a0d
José Rivero <jorive@microsoft.com>
2019-08-06 10:01:49 -0700

    Update src/Tools/dotnet-trace/CommandLine/Commands/CollectCommand.cs
    Co-Authored-By: Noah Falk <noahfalk@users.noreply.github.com>

--- src/Tools/dotnet-trace/CommandLine/Commands/CollectCommand.cs
+++ src/Tools/dotnet-trace/CommandLine/Commands/CollectCommand.cs
@@ -28,7 +28,7 @@ namespace Microsoft.Diagnostics.Tools.Trace
         /// <param name="processId">The process to collect the trace from.</param>
         /// <param name="output">The output path for the collected trace data.</param>
         /// <param name="buffersize">Sets the size of the in-memory circular buffer in megabytes.</param>
-        /// <param name="providers">A list of EventPipe providers to be enabled. This is in the form 'Provider[,Provider]', where Provider is in the form: '(KnownProviderName)[:Flags[:Level][:KeyValueArgs]]', and KeyValueArgs is in the form: '[key1=value1][;key2=value2]'</param>
+        /// <param name="providers">A list of EventPipe providers to be enabled. This is in the form 'Provider[,Provider]', where Provider is in the form: 'KnownProviderName[:Flags[:Level][:KeyValueArgs]]', and KeyValueArgs is in the form: '[key1=value1][;key2=value2]'</param>
         /// <param name="profile">A named pre-defined set of provider configurations that allows common tracing scenarios to be specified succinctly.</param>
         /// <param name="format">The desired format of the created trace file.</param>
         /// <returns></returns>
