e063601a81c19a1796c347cebbe564f807f0f247
Sung Yoon Whang <suwhang@microsoft.com>
2019-08-16 13:16:17 -0700

    Fix IncrementingCounter payload generation (#433)
    * Fix IncrementingCounter payload generation
    
    * code review

--- src/Tools/dotnet-counters/CounterPayload.cs
+++ src/Tools/dotnet-counters/CounterPayload.cs
@@ -50,12 +50,16 @@ namespace Microsoft.Diagnostics.Tools.Counters
         public string m_Value;
         public string m_DisplayName;
         public string m_DisplayRateTimeScale;
-        public IncrementingCounterPayload(IDictionary<string, object> payloadFields)
+        public IncrementingCounterPayload(IDictionary<string, object> payloadFields, int interval)
         {
             m_Name = payloadFields["Name"].ToString();
             m_Value = payloadFields["Increment"].ToString();
             m_DisplayName = payloadFields["DisplayName"].ToString();
-            m_DisplayRateTimeScale = TimeSpan.Parse(payloadFields["DisplayRateTimeScale"].ToString()).ToString("%s' sec'");
+            m_DisplayRateTimeScale = payloadFields["DisplayRateTimeScale"].ToString();
+
+            // In case these properties are not provided, set them to appropriate values.
+            m_DisplayName = m_DisplayName.Length == 0 ? m_Name : m_DisplayName;
+            m_DisplayRateTimeScale = m_DisplayRateTimeScale.Length == 0 ? $"{interval} sec" : TimeSpan.Parse(m_DisplayRateTimeScale).ToString("%s' sec'");
         }
 
         public string GetName()
