af8a24e1cbb97d637b4468489204deb50fd0cd3b
Sung Yoon Whang <suwhang@microsoft.com>
2019-09-23 17:56:47 -0700

    Remove ICounterExporter

--- src/Tools/dotnet-counters/Exporters/ICounterExporter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.Diagnostics.Tools.Counters.Exporters
-{
-    public interface ICounterExporter
-    {
-        void Write(string providerName, ICounterPayload counterPayload);
-        void Initialize(string _output, string processName);
-        void Flush();
-    }
-}
