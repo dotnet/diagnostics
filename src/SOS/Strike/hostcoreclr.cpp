667723e07372492b89ab8b711bd5953f9529e94b
Mike McLaughlin <mikem@microsoft.com>
2019-10-02 10:25:39 -0700

    Display internal SOS status with sosstatus command (#519)
    Display internal SOS status with sosstatus command
    
    Add dumpvc to dotnet-dump/lldb commands.
    
    Remove reductant dotnet-dump error message when SOS command fails.
    
    Fixes issues:
    
        https://github.com/dotnet/diagnostics/issues/511
        https://github.com/dotnet/diagnostics/issues/508
        https://github.com/dotnet/diagnostics/issues/509
    
    Change SyncBlk test not to run on xplat.

--- src/SOS/Strike/hostcoreclr.cpp
+++ src/SOS/Strike/hostcoreclr.cpp
@@ -30,7 +30,6 @@
 
 #include <coreclrhost.h>
 #include <set>
-#include <string>
 
 #if defined(__APPLE__)
 #include <mach-o/dyld.h>
@@ -231,7 +230,7 @@ static bool GetEntrypointExecutableAbsolutePath(std::string& entrypointExecutabl
 /**********************************************************************\
  * Returns the coreclr module/runtime directory of the target.
 \**********************************************************************/
-static HRESULT GetCoreClrDirectory(std::string& coreClrDirectory)
+HRESULT GetCoreClrDirectory(std::string& coreClrDirectory)
 {
 #ifdef FEATURE_PAL
     LPCSTR directory = g_ExtServices->GetCoreClrDirectory();
