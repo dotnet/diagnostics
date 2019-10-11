4765e5bddb6d1303c829276357c11fc9987a373a
Sung Yoon Whang <suwhang@microsoft.com>
2019-10-10 19:25:47 -0700

    tabs -> spaces

--- src/Tools/dotnet-counters/Exporters/ICounterRenderer.cs
+++ src/Tools/dotnet-counters/Exporters/ICounterRenderer.cs
@@ -6,10 +6,10 @@ namespace Microsoft.Diagnostics.Tools.Counters.Exporters
 {
     public interface ICounterRenderer
     {
-		void Initialize();
-		void EventPipeSourceConnected();
-		void ToggleStatus(bool paused);
-		void CounterPayloadReceived(string providerName, ICounterPayload payload, bool paused);
-		void Stop();
+        void Initialize();
+        void EventPipeSourceConnected();
+        void ToggleStatus(bool paused);
+        void CounterPayloadReceived(string providerName, ICounterPayload payload, bool paused);
+        void Stop();
     }
 }
