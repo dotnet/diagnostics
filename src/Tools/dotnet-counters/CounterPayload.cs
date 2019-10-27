ed400df374038e6f5694056d560b49579374870e
Sung Yoon Whang <suwhang@microsoft.com>
2019-10-26 19:30:10 -0700

    Fix incorrect value displayed for rate counters (#583)
    * Fix incorrect value displayed for rate counters
    
    * Add tests

--- src/Tools/dotnet-counters/CounterPayload.cs
+++ src/Tools/dotnet-counters/CounterPayload.cs
@@ -52,20 +52,24 @@ namespace Microsoft.Diagnostics.Tools.Counters
 
     class IncrementingCounterPayload : ICounterPayload
     {
+        private int timescaleInSec;
         public string m_Name;
         public double m_Value;
         public string m_DisplayName;
         public string m_DisplayRateTimeScale;
+
         public IncrementingCounterPayload(IDictionary<string, object> payloadFields, int interval)
         {
             m_Name = payloadFields["Name"].ToString();
             m_Value = (double)payloadFields["Increment"];
             m_DisplayName = payloadFields["DisplayName"].ToString();
             m_DisplayRateTimeScale = payloadFields["DisplayRateTimeScale"].ToString();
+            timescaleInSec = m_DisplayRateTimeScale.Length == 0 ? 1 : (int)TimeSpan.Parse(m_DisplayRateTimeScale).TotalSeconds;
+            m_Value *= timescaleInSec;
 
             // In case these properties are not provided, set them to appropriate values.
             m_DisplayName = m_DisplayName.Length == 0 ? m_Name : m_DisplayName;
-            m_DisplayRateTimeScale = m_DisplayRateTimeScale.Length == 0 ? $"{interval} sec" : TimeSpan.Parse(m_DisplayRateTimeScale).ToString("%s' sec'");
+            m_DisplayRateTimeScale = m_DisplayRateTimeScale.Length == 0 ? $"{interval} sec" : $"{timescaleInSec} sec";
         }
 
         public string GetName()
