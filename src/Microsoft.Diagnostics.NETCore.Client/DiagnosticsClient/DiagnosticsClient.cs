f8667ff67a1327eb433ce3a46cf0333b53e7e284
Sung Yoon Whang <suwhang@microsoft.com>
2019-11-06 16:39:31 -0800

    Implement WriteDump

--- src/Microsoft.Diagnostics.NETCore.Client/DiagnosticsClient/DiagnosticsClient.cs
+++ src/Microsoft.Diagnostics.NETCore.Client/DiagnosticsClient/DiagnosticsClient.cs
@@ -4,6 +4,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Microsoft.Diagnostics.NETCore.Client
 {
@@ -39,9 +41,29 @@ namespace Microsoft.Diagnostics.NETCore.Client
         /// <param name="dumpType">Type of the dump to be generated</param>
         /// <param name="dumpPath">Full path to the dump to be generated. By default it is /tmp/coredump.{pid}</param>
         /// <param name="logDumpGeneration">When set to true, display the dump generation debug log to the console.</param>
-        public void WriteDump(DumpType dumpType, string dumpPath=null, bool logDumpGeneration=false)
+        public void WriteDump(DumpType dumpType, string dumpPath, bool logDumpGeneration=false)
         {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                throw new PlatformNotSupportedException($"Unsupported operating system: {RuntimeInformation.OSDescription}");
 
+            if (string.IsNullOrEmpty(dumpPath))
+                throw new ArgumentNullException($"{nameof(dumpPath)} required");
+
+            var payload = SerializeCoreDump(dumpPath, dumpType, logDumpGeneration);
+            var message = new IpcMessage(DiagnosticsServerCommandSet.Dump, (byte)DumpCommandId.GenerateCoreDump, payload);
+            var response = IpcClient.SendMessage(_processId, message);
+            var hr = 0;
+            switch ((DiagnosticsServerCommandId)response.Header.CommandId)
+            {
+                case DiagnosticsServerCommandId.Error:
+                case DiagnosticsServerCommandId.OK:
+                    hr = BitConverter.ToInt32(response.Payload, 0);
+                    break;
+                default:
+                    return;
+            }
+
+            return;
         }
 
         /// <summary>
@@ -67,6 +89,19 @@ namespace Microsoft.Diagnostics.NETCore.Client
             return null;
 
         }
-    }
 
+        private static byte[] SerializeCoreDump(string dumpName, DumpType dumpType, bool diagnostics)
+        {
+            using (var stream = new MemoryStream())
+            using (var writer = new BinaryWriter(stream))
+            {
+                writer.WriteString(dumpName);
+                writer.Write((uint)dumpType);
+                writer.Write((uint)(diagnostics ? 1 : 0));
+
+                writer.Flush();
+                return stream.ToArray();
+            }
+        }
+    }
 }
