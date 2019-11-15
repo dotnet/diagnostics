bec51df6ed410921743702ed11726ff5dfba2968
Sung Yoon Whang <suwhang@microsoft.com>
2019-11-14 18:09:56 -0800

    Add test for DiagnosticsClient library

---
+++ src/tests/Microsoft.Diagnostics.NETCore.Client/BasicTests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Diagnostics;
+using Xunit;
+
+using Microsoft.Diagnostics.NETCore.Client;
+
+namespace Microsoft.Diagnostics.NETCore.Client
+{
+
+    /// <summary>
+    /// Suite of tests that test top-level commands
+    /// </summary>
+    public class BasicTests
+    {
+        [Fact]
+        public void ConnectTest1()
+        {
+            
+        }
+    }
+}
