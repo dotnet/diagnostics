3697a540b5a9d28d7838a56d532900803d3a0bb9
Sung Yoon Whang <suwhang@microsoft.com>
2019-11-18 13:51:36 -0800

    Add Tracee

---
+++ src/tests/Tracee/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Tracee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello World!");
+                Thread.Sleep(500);
+            }
+            Thread.Sleep(5000);
+        }
+    }
+}
