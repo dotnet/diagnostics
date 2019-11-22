bd514e3903280bf855f37d2aa244edb92310d2b6
Sung Yoon Whang <suwhang@microsoft.com>
2019-11-22 14:59:37 -0800

    Try using dotnet host

--- src/tests/newtest/UnitTest1.cs
+++ src/tests/newtest/UnitTest1.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Runtime.InteropServices;
 using Xunit;
 
+using Microsoft.Diagnostics.TestHelpers;
+
 namespace newtest
 {
     public class UnitTest1
@@ -14,15 +16,34 @@ namespace newtest
             var traceePath = curPath.Replace("newtest.UnitTests", "Tracee");
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                return traceePath + "/Tracee.exe";
+                return traceePath + "\\Tracee.dll";
             }
-            return traceePath + "/Tracee";
+            else
+            {
+                return traceePath + "/Tracee.dll";
+            }
+        }
+
+        public string GetHostExePath()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var repoRoot = "..\\..\\..\\..\\..";
+                var dotnetHostExe = "\\.dotnet\\dotnet.exe";
+                return repoRoot + dotnetHostExe;
+            }
+            else
+            {
+                var repoRoot = "../../../../..";
+                var dotnetHostExe = "/.dotnet/dotnet";
+                return repoRoot + dotnetHostExe;
+            }
         }
 
         [Fact]
         public void ProcessLaunchTest1()
         {
-            ProcessStartInfo startInfo = new ProcessStartInfo(GetTraceePath());
+            ProcessStartInfo startInfo = new ProcessStartInfo(GetHostExePath(), GetTraceePath());
             startInfo.UseShellExecute = false;
             startInfo.RedirectStandardOutput = true;
             Process process = Process.Start(startInfo);
