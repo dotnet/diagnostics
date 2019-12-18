5730edfed7e3ee06772e9195f380c16a03b9cfc8
Andrew Au <andrewau@microsoft.com>
2019-12-18 10:38:52 -0800

    Avoid NullReferenceException in case the code path is run under an incompatible runtime

--- src/Microsoft.Diagnostics.NETCore.Client/DiagnosticsIpc/IpcClient.cs
+++ src/Microsoft.Diagnostics.NETCore.Client/DiagnosticsIpc/IpcClient.cs
@@ -128,6 +128,10 @@ namespace Microsoft.Diagnostics.NETCore.Client
 #elif NETSTANDARD2_0
             // UnixDomainSocketEndPoint is not part of .NET Standard 2.0
             var type = typeof(Socket).Assembly.GetType("System.Net.Sockets.UnixDomainSocketEndPoint");
+            if (type == null)
+            {
+                throw new PlatformNotSupportedException("Current process is not running a compatible .NET Core runtime.");
+            }
             var ctor = type.GetConstructor(new[] { typeof(string) });
             return (EndPoint)ctor.Invoke(new object[] { path });
 #endif
