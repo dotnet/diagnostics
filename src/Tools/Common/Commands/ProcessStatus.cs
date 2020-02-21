e0590edad5c95bd9e4a206918072a903fed34a07
Aniruddha Achar B P <anacharb@microsoft.com>
2020-02-22 00:07:32 +0530

    Build failed. Removed namespace that was not used.

--- src/Tools/Common/Commands/ProcessStatus.cs
+++ src/Tools/Common/Commands/ProcessStatus.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.Diagnostics.NETCore.Client;
-using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
 using System;
 using System.Collections.Generic;
 using System.CommandLine;
