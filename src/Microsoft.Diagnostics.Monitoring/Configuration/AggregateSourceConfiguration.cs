55d5a20964f3ec990c6fe97b607161cda1a191e2
Justin Anderson <jander-msft@users.noreply.github.com>
2020-05-13 10:45:43 -0700

    Split HTTP request configuration from logging configuration. (#1130)
    * Split HTTP request configuration from logging configuration.
    * Fix HTTP request filter string to not be doubly double quoted.

---
+++ src/Microsoft.Diagnostics.Monitoring/Configuration/AggregateSourceConfiguration.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Diagnostics.NETCore.Client;
+
+namespace Microsoft.Diagnostics.Monitoring
+{
+    public sealed class AggregateSourceConfiguration : MonitoringSourceConfiguration
+    {
+        private IList<MonitoringSourceConfiguration> _configurations;
+
+        public AggregateSourceConfiguration(params MonitoringSourceConfiguration[] configurations)
+        {
+            _configurations = configurations;
+        }
+
+        public override IList<EventPipeProvider> GetProviders()
+        {
+            // CONSIDER: Might have to deduplicate providers and merge them together.
+            return _configurations.SelectMany(c => c.GetProviders()).ToList();
+        }
+    }
+}
