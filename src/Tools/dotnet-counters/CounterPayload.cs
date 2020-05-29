dbf9b995913bb271d9fce105ecde16645b5bb7db
Sung Yoon Whang <suwhang@microsoft.com>
2020-05-28 17:57:30 -0700

    make dotnet-counters not extrapolate the incoming values

--- src/Tools/dotnet-counters/CounterPayload.cs
+++ src/Tools/dotnet-counters/CounterPayload.cs
@@ -62,11 +62,10 @@ namespace Microsoft.Diagnostics.Tools.Counters
 
     class IncrementingCounterPayload : ICounterPayload
     {
-        private int timescaleInSec;
         public string m_Name;
         public double m_Value;
         public string m_DisplayName;
-        public string m_DisplayRateTimeScale;
+        public string m_Interval;
         public string m_DisplayUnits;
 
         public IncrementingCounterPayload(IDictionary<string, object> payloadFields, int interval)
@@ -74,14 +73,11 @@ namespace Microsoft.Diagnostics.Tools.Counters
             m_Name = payloadFields["Name"].ToString();
             m_Value = (double)payloadFields["Increment"];
             m_DisplayName = payloadFields["DisplayName"].ToString();
-            m_DisplayRateTimeScale = payloadFields["DisplayRateTimeScale"].ToString();
             m_DisplayUnits = payloadFields["DisplayUnits"].ToString();
-            timescaleInSec = m_DisplayRateTimeScale.Length == 0 ? 1 : (int)TimeSpan.Parse(m_DisplayRateTimeScale).TotalSeconds;
-            m_Value *= timescaleInSec;
+            m_Interval = interval.ToString() + " sec";
 
             // In case these properties are not provided, set them to appropriate values.
             m_DisplayName = m_DisplayName.Length == 0 ? m_Name : m_DisplayName;
-            m_DisplayRateTimeScale = m_DisplayRateTimeScale.Length == 0 ? $"{interval} sec" : $"{timescaleInSec} sec";
         }
 
         public string GetName()
@@ -97,9 +93,9 @@ namespace Microsoft.Diagnostics.Tools.Counters
         public string GetDisplay()
         {
             if (m_DisplayUnits.Length > 0)
-                return $"{m_DisplayName} / {m_DisplayRateTimeScale} ({m_DisplayUnits})";
+                return $"{m_DisplayName} / {m_Interval} ({m_DisplayUnits})";
 
-            return $"{m_DisplayName} / {m_DisplayRateTimeScale}";
+            return $"{m_DisplayName} / {m_Interval}";
         }
 
         public string GetCounterType()
