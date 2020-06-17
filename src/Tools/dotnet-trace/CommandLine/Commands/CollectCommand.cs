7e4a09c78a144ff4f4ef607c67b877da94c5c4a1
Sung Yoon Whang <suwhang@microsoft.com>
2020-06-17 15:47:08 -0700

    respect --buffersize flag

--- src/Tools/dotnet-trace/CommandLine/Commands/CollectCommand.cs
+++ src/Tools/dotnet-trace/CommandLine/Commands/CollectCommand.cs
@@ -144,7 +144,7 @@ namespace Microsoft.Diagnostics.Tools.Trace
                     EventPipeSession session = null;
                     try
                     {
-                        session = diagnosticsClient.StartEventPipeSession(providerCollection, true);
+                        session = diagnosticsClient.StartEventPipeSession(providerCollection, true, (int)buffersize);
                     }
                     catch (DiagnosticsClientException e)
                     {
