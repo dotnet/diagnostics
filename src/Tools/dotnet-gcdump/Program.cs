1387e4ce056cc024b0bc8777dad824e9e0492898
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-07-28 09:00:48 +0200

    Add missing "for" word

--- src/Tools/dotnet-gcdump/Program.cs
+++ src/Tools/dotnet-gcdump/Program.cs
@@ -15,7 +15,7 @@ namespace Microsoft.Diagnostics.Tools.GCDump
         {
             var parser = new CommandLineBuilder()
                 .AddCommand(CollectCommandHandler.CollectCommand())
-                .AddCommand(ProcessStatusCommandHandler.ProcessStatusCommand("Lists the dotnet processes that gcdumps can be collected"))
+                .AddCommand(ProcessStatusCommandHandler.ProcessStatusCommand("Lists the dotnet processes that gcdumps can be collected for"))
                 .AddCommand(ReportCommandHandler.ReportCommand())
                 .UseDefaults()
                 .Build();
