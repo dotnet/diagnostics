773a829493bd6bcc9694272f6c4b88f386faea91
Justin Anderson <jander-msft@users.noreply.github.com>
2020-08-07 12:07:24 -0700

    Build dotnet-monitor tool as .NET Core 3.1 application.

--- src/Tools/dotnet-monitor/Startup.cs
+++ src/Tools/dotnet-monitor/Startup.cs
@@ -2,19 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Generic;
 using System.IO.Compression;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Diagnostics.Monitoring.RestServer;
+using Microsoft.Diagnostics.Monitoring.RestServer.Controllers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.Diagnostics.Monitoring
 {
@@ -30,15 +28,14 @@ namespace Microsoft.Diagnostics.Monitoring
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc((MvcOptions options) =>
-            {
-                options.Filters.Add(new ProducesAttribute("application/json"));
+            services.AddMvc(options =>
+                {
+                    options.Filters.Add(new ProducesAttribute("application/json"));
 
-                // HACK We need to disable EndpointRouting in order to run properly in 3.1
-                System.Reflection.PropertyInfo prop = options.GetType().GetProperty("EnableEndpointRouting");
-                prop?.SetValue(options, false);
-            }).SetCompatibilityVersion(CompatibilityVersion.Latest);
-
+                    options.EnableEndpointRouting = false;
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Latest)
+                .AddApplicationPart(typeof(DiagController).Assembly);
             services.Configure<ApiBehaviorOptions>(options =>
             {
                 options.InvalidModelStateResponseFactory = context =>
@@ -71,7 +68,7 @@ namespace Microsoft.Diagnostics.Monitoring
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
