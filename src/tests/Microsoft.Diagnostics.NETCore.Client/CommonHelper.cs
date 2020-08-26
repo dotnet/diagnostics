ef8edf2c6876c68613f8c5f65c47a7360612e0a4
John Salem <josalem@microsoft.com>
2020-08-26 12:02:22 -0700

    TestRunner uess newest DARC version

--- src/tests/Microsoft.Diagnostics.NETCore.Client/CommonHelper.cs
+++ src/tests/Microsoft.Diagnostics.NETCore.Client/CommonHelper.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.IO;
 using System.Runtime.InteropServices;
 
@@ -9,6 +10,9 @@ namespace Microsoft.Diagnostics.NETCore.Client
 {
     public class CommonHelper
     {
+        // This is a stopgap until Arcade updates to an RC version of the SDK.
+        // KEEP IN SYNC WITH eng/Versions.xml
+        public static readonly string CurrentDARCVersion = "5.0.0-rc.1.20425.1";
         public static string HostExe = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? 
             (RuntimeInformation.ProcessArchitecture == Architecture.X86 ? 
                 "..\\..\\..\\..\\..\\.dotnet\\x86\\dotnet.exe" : 
@@ -18,12 +22,17 @@ namespace Microsoft.Diagnostics.NETCore.Client
         public static string GetTraceePath(string traceeName = "Tracee", string targetFramework = "netcoreapp3.1")
         {
             var curPath = Directory.GetCurrentDirectory();
-;
+
             var traceePath = curPath
                 .Replace(System.Reflection.Assembly.GetCallingAssembly().GetName().Name, traceeName)
                 .Replace("netcoreapp3.1", targetFramework);
 
-            return Path.Combine(traceePath, Path.ChangeExtension(traceeName, ".dll"));
+            traceePath = Path.Combine(traceePath, Path.ChangeExtension(traceeName, ".dll"));
+
+            if (targetFramework.Equals("net5.0", StringComparison.InvariantCultureIgnoreCase))
+                traceePath = $"--fx-version {CurrentDARCVersion} {traceePath}";
+
+            return traceePath;
         }
     }
 }
