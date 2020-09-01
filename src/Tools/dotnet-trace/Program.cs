f9d96f716dec9fc755ab358c27cab184d550db55
Sung Yoon Whang <suwhang@microsoft.com>
2020-09-01 14:18:02 -0700

    Add trace run command

--- src/Tools/dotnet-trace/Program.cs
+++ src/Tools/dotnet-trace/Program.cs
@@ -18,6 +18,7 @@ namespace Microsoft.Diagnostics.Tools.Trace
                 .AddCommand(ProcessStatusCommandHandler.ProcessStatusCommand("Lists the dotnet processes that traces can be collected"))
                 .AddCommand(ListProfilesCommandHandler.ListProfilesCommand())
                 .AddCommand(ConvertCommandHandler.ConvertCommand())
+                .AddCommand(RunCommandHandler.RunCommand())
                 .UseDefaults()
                 .Build();
 
