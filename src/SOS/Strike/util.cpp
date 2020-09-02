524088971e0462c62569cc290dca9629447c3bb6
Mike McLaughlin <mikem@microsoft.com>
2020-09-01 17:28:27 -0700

    Fix SOS crashes in !dumpstack and !eestack (#1519)
    Fix module version search and off-by-1 in module names API. Other misc cleanup.

--- src/SOS/Strike/util.cpp
+++ src/SOS/Strike/util.cpp
@@ -4140,7 +4140,7 @@ HRESULT ReadVirtualCache::Read(TADDR address, PVOID buffer, ULONG bufferSize, PU
         return g_ExtData->ReadVirtual(TO_CDADDR(address), buffer, bufferSize, lpcbBytesRead);
     }
 
-    if (!m_cacheValid || (address < m_startCache) || (address > (m_startCache + (m_cacheSize - bufferSize))))
+    if (!m_cacheValid || (address < m_startCache) || (address > (m_startCache + m_cacheSize - bufferSize)))
     {
         ULONG cbBytesRead = 0;
 
@@ -4161,12 +4161,21 @@ HRESULT ReadVirtualCache::Read(TADDR address, PVOID buffer, ULONG bufferSize, PU
         m_cacheValid = TRUE;
     }
 
-    int size = _min(bufferSize, m_cacheSize);
-    memcpy(buffer, (LPVOID) ((ULONG64)m_cache + (address - m_startCache)), size);
-
-    if (lpcbBytesRead != NULL)
+    // If the address is within the cache, copy the cached memory to the input buffer
+    LONG_PTR cacheOffset = address - m_startCache;
+    if (cacheOffset >= 0 && cacheOffset < CACHE_SIZE)
     {
-        *lpcbBytesRead = size;
+        int size = _min(bufferSize, m_cacheSize);
+        memcpy(buffer, (LPVOID)(m_cache + cacheOffset), size);
+
+        if (lpcbBytesRead != NULL)
+        {
+            *lpcbBytesRead = size;
+        }
+    }
+    else
+    {
+        return E_FAIL;
     }
 
     return S_OK;
