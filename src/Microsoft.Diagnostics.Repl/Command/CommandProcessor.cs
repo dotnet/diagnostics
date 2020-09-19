36a29910386e1add672a1f0211d6c983b9e656ae
Mike McLaughlin <mikem@microsoft.com>
2020-09-19 12:46:07 -0700

    Update to latest System.CommandLine (2.0.0-beta1.20467.2) (#1586)
    Remove workaround in command processor.
    
    This update also fixes option aliases like "lm -v".

--- src/Microsoft.Diagnostics.Repl/Command/CommandProcessor.cs
+++ src/Microsoft.Diagnostics.Repl/Command/CommandProcessor.cs
@@ -117,10 +117,6 @@ namespace Microsoft.Diagnostics.Repl
             {
                 if (baseAttribute is CommandAttribute commandAttribute && IsValidPlatform(commandAttribute))
                 {
-                    if (command != null)
-                    {
-                        rootBuilder.AddCommand(command);
-                    }
                     command = new Command(commandAttribute.Name, commandAttribute.Help);
                     var properties = new List<(PropertyInfo, Option)>();
                     var arguments = new List<(PropertyInfo, Argument)>();
@@ -168,6 +164,7 @@ namespace Microsoft.Diagnostics.Repl
                     var handler = new Handler(this, commandAttribute.AliasExpansion, arguments, properties, type);
                     _commandHandlers.Add(command.Name, handler);
                     command.Handler = handler;
+                    rootBuilder.AddCommand(command);
                 }
 
                 if (baseAttribute is CommandAliasAttribute commandAliasAttribute && IsValidPlatform(commandAliasAttribute))
@@ -178,11 +175,6 @@ namespace Microsoft.Diagnostics.Repl
                     }
                     command.AddAlias(commandAliasAttribute.Name);
                 }
-            }
-
-            if (command != null)
-            {
-                rootBuilder.AddCommand(command);
             }
         }
 
