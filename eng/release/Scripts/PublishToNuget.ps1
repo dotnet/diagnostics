713de7b7cdeabf32546cba7d8fa2ea66c1fb9b60
Juan Sebastian Hoyos Ayala <juhoyosa@microsoft.com>
2020-10-20 03:00:20 -0700

    Change nuget publishing script

--- eng/release/Scripts/PublishToNuget.ps1
+++ eng/release/Scripts/PublishToNuget.ps1
@@ -25,13 +25,13 @@ if ($help -or (($null -ne $properties) -and ($properties.Contains('/help') -or $
 }
 
 if ($null -ne $properties) {
-    Write-Host "Unexpected extra parameters: $properties."
+    Write-Error "Unexpected extra parameters: $properties."
     exit 1
 }
 
 if (!(Test-Path $ManifestPath))
 {
-    Write-Host "Error: unable to find maifest at $ManifestPath."
+    Write-Error "Error: unable to find maifest at $ManifestPath."
 }
 
 $manifestSize = $(Get-ChildItem $ManifestPath).length / 1kb
@@ -39,7 +39,7 @@ $manifestSize = $(Get-ChildItem $ManifestPath).length / 1kb
 # Limit size. For large manifests
 if ($manifestSize -gt 500) 
 {
-    Write-Host "Error: Manifest $ManifestPath too large."
+    Write-Error "Error: Manifest $ManifestPath too large."
 }
 
 $manifestJson = Get-Content -Raw -Path $ManifestPath | ConvertFrom-Json
@@ -47,17 +47,23 @@ $manifestJson = Get-Content -Raw -Path $ManifestPath | ConvertFrom-Json
 $failedToPublish = 0
 foreach ($nugetPack in $manifestJson.NugetAssets)
 {
+    $progressPreference = 'silentlyContinue'
     $packagePath = Join-Path $StagingPath $nugetPack.PublishRelativePath
+    Write-Host "Downloading: $nugetPack."
+    Invoke-WebRequest $nugetPack.PublishedPath -OutFile (New-Item -Path $packagePath -Force)
+    $progressPreference = 'Continue'
+
+    Write-Host "Publishing $packagePath."
     & "$PSScriptRoot/../../../dotnet.cmd" nuget push $packagePath --source $FeedEndpoint --api-key $FeedPat
     if ($LastExitCode -ne 0)
     {
-        Write-Host "Error: unable to publish $($nugetPack.FilePath)."
+        Write-Error "Error: unable to publish $($nugetPack.FilePath)."
         $failedToPublish++
     }
 }
 
 if ($LastExitCode -ne 0)
 {
-    Write-Host "Error: $failedToPublish packages unpublished."
+    Write-Error "Error: $failedToPublish packages unpublished."
     exit 1
 }
