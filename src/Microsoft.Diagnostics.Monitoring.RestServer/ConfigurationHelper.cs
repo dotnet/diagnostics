956a84bb17105adcf22e796e9d647e56a8294174
Wiktor Kopec <wiktork@microsoft.com>
2020-12-03 10:24:14 -0800

    Change default binding to https. - Allow partial success for missing cert.

---
+++ src/Microsoft.Diagnostics.Monitoring.RestServer/ConfigurationHelper.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Diagnostics.Monitoring.RestServer
+{
+    internal static class ConfigurationHelper
+    {
+        public static string MakeKey(string parent, string child)
+        {
+            return FormattableString.Invariant($"{parent}:{child}");
+        }
+
+        public static string[] SplitValue(string value)
+        {
+            return value.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
--- src/Tools/dotnet-monitor/DiagnosticsMonitorCommandHandler.cs
+++ src/Tools/dotnet-monitor/DiagnosticsMonitorCommandHandler.cs
@@ -4,6 +4,8 @@
 
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Diagnostics.Monitoring;
 using Microsoft.Diagnostics.Monitoring.RestServer;
 using Microsoft.Extensions.Configuration;
@@ -13,8 +15,10 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.CommandLine;
+using System.CommandLine.IO;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -55,21 +59,13 @@ namespace Microsoft.Diagnostics.Tools.Monitor
 
         public IHostBuilder CreateHostBuilder(IConsole console, string[] urls, string[] metricUrls, bool metrics, string diagnosticPort)
         {
-            if (metrics)
-            {
-                urls = urls.Concat(metricUrls).ToArray();
-            }
-
             return Host.CreateDefaultBuilder()
                 .UseContentRoot(AppContext.BaseDirectory) // Use the application root instead of the current directory
                 .ConfigureAppConfiguration((IConfigurationBuilder builder) =>
                 {
                     //Note these are in precedence order.
                     ConfigureEndpointInfoSource(builder, diagnosticPort);
-                    if (metrics)
-                    {
-                        ConfigureMetricsEndpoint(builder, metricUrls);
-                    }
+                    ConfigureMetricsEndpoint(builder, metrics, metricUrls);
 
                     builder.AddJsonFile(UserSettingsPath, optional: true, reloadOnChange: true);
                     builder.AddJsonFile(SharedSettingsPath, optional: true, reloadOnChange: true);
@@ -85,10 +81,7 @@ namespace Microsoft.Diagnostics.Tools.Monitor
                     services.AddHostedService<FilteredEndpointInfoSourceHostedService>();
                     services.AddSingleton<IDiagnosticServices, DiagnosticServices>();
                     services.ConfigureEgress(context.Configuration);
-                    if (metrics)
-                    {
-                        services.ConfigureMetrics(context.Configuration);
-                    }
+                    services.ConfigureMetrics(context.Configuration);
                     services.AddSingleton<ExperimentalToolLogger>();
                 })
                 .ConfigureLogging(builder =>
@@ -98,19 +91,99 @@ namespace Microsoft.Diagnostics.Tools.Monitor
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls(urls);
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel((context, options) =>
+                    {
+                        //Note our priorities for hosting urls don't match the default behavior.
+                        //Default Kestrel behavior priority
+                        //1) ConfigureKestrel settings
+                        //2) Command line arguments (--urls)
+                        //3) Environment variables (ASPNETCORE_URLS, then DOTNETCORE_URLS)
+
+                        //Our precedence
+                        //1) Environment variables (ASPNETCORE_URLS, DotnetMonitor_Metrics__Endpoints)
+                        //2) Command line arguments (these have defaults) --urls, --metricUrls
+                        //3) ConfigureKestrel is used for fine control of the server, but honors the first two configurations.
+
+                        string hostingUrl = context.Configuration.GetValue<string>(WebHostDefaults.ServerUrlsKey);
+                        if (!string.IsNullOrEmpty(hostingUrl))
+                        {
+                            urls = ConfigurationHelper.SplitValue(hostingUrl);
+                        }
+
+                        var metricsOptions = new MetricsOptions();
+                        context.Configuration.Bind(MetricsOptions.ConfigurationKey, metricsOptions);
+
+                        if (metricsOptions.Enabled)
+                        {
+                            string metricUrlFromConfig = metricsOptions.Endpoints;
+                            if (!string.IsNullOrEmpty(metricUrlFromConfig))
+                            {
+                                metricUrls = ConfigurationHelper.SplitValue(metricUrlFromConfig);
+                            }
+
+                            urls = urls.Concat(metricUrls).ToArray();
+                        }
+
+                        bool boundListeningPort = false;
+
+                        //Workaround for lack of default certificate. See https://github.com/dotnet/aspnetcore/issues/28120
+                        options.Configure(context.Configuration.GetSection("Kestrel")).Load();
+
+                        //By default, we bind to https for sensitive data (such as dumps and traces) and bind http for
+                        //non-sensitive data such as metrics. We may be missing a certificate for https binding. We want to continue with the
+                        //http binding in that scenario.
+                        foreach (BindingAddress url in urls.Select(BindingAddress.Parse))
+                        {
+                            Action<ListenOptions> configureListenOptions = (listenOptions) =>
+                            {
+                                if (url.Scheme.Equals(Uri.UriSchemeHttps, StringComparison.OrdinalIgnoreCase))
+                                {
+                                    listenOptions.UseHttps();
+                                }
+                            };
+
+                            try
+                            {
+                                if (url.Host.Equals("localhost", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    options.ListenLocalhost(url.Port, configureListenOptions);
+                                }
+                                else if (IPAddress.TryParse(url.Host, out IPAddress ipAddress))
+                                {
+                                    options.Listen(ipAddress, url.Port, configureListenOptions);
+                                }
+                                else
+                                {
+                                    options.ListenAnyIP(url.Port, configureListenOptions);
+                                }
+                                boundListeningPort = true;
+                            }
+                            catch (InvalidOperationException e)
+                            {
+                                //This binding failure is typically due to missing default certificate
+                                console.Error.WriteLine($"Unable to bind to {url}. Dotnet-monitor functionality will be limited.");
+                                console.Error.WriteLine(e.Message);
+                            }
+                        }
+
+                        //If we end up not binding any ports, Kestrel defaults to port 5000. Make sure we don't attempt this.
+                        if (!boundListeningPort)
+                        {
+                            throw new InvalidOperationException("Unable to bind any urls.");
+                        }
+                    })
+                    .UseStartup<Startup>();
                 });
         }
 
-        private static void ConfigureMetricsEndpoint(IConfigurationBuilder builder, string[] metricEndpoints)
+        private static void ConfigureMetricsEndpoint(IConfigurationBuilder builder, bool enableMetrics, string[] metricEndpoints)
         {
             builder.AddInMemoryCollection(new Dictionary<string, string>
             {
-                {MakeKey(MetricsOptions.ConfigurationKey, nameof(MetricsOptions.Endpoints)), string.Join(';', metricEndpoints)},
-                {MakeKey(MetricsOptions.ConfigurationKey, nameof(MetricsOptions.Enabled)), true.ToString()},
-                {MakeKey(MetricsOptions.ConfigurationKey, nameof(MetricsOptions.UpdateIntervalSeconds)), 10.ToString()},
-                {MakeKey(MetricsOptions.ConfigurationKey, nameof(MetricsOptions.MetricCount)), 3.ToString()}
+                {ConfigurationHelper.MakeKey(MetricsOptions.ConfigurationKey, nameof(MetricsOptions.Endpoints)), string.Join(';', metricEndpoints)},
+                {ConfigurationHelper.MakeKey(MetricsOptions.ConfigurationKey, nameof(MetricsOptions.Enabled)), enableMetrics.ToString()},
+                {ConfigurationHelper.MakeKey(MetricsOptions.ConfigurationKey, nameof(MetricsOptions.UpdateIntervalSeconds)), 10.ToString()},
+                {ConfigurationHelper.MakeKey(MetricsOptions.ConfigurationKey, nameof(MetricsOptions.MetricCount)), 3.ToString()}
             });
         }
 
@@ -119,14 +192,9 @@ namespace Microsoft.Diagnostics.Tools.Monitor
             DiagnosticPortConnectionMode connectionMode = string.IsNullOrEmpty(diagnosticPort) ? DiagnosticPortConnectionMode.Connect : DiagnosticPortConnectionMode.Listen;
             builder.AddInMemoryCollection(new Dictionary<string, string>
             {
-                {MakeKey(DiagnosticPortOptions.ConfigurationKey, nameof(DiagnosticPortOptions.ConnectionMode)), connectionMode.ToString()},
-                {MakeKey(DiagnosticPortOptions.ConfigurationKey, nameof(DiagnosticPortOptions.EndpointName)), diagnosticPort}
+                {ConfigurationHelper.MakeKey(DiagnosticPortOptions.ConfigurationKey, nameof(DiagnosticPortOptions.ConnectionMode)), connectionMode.ToString()},
+                {ConfigurationHelper.MakeKey(DiagnosticPortOptions.ConfigurationKey, nameof(DiagnosticPortOptions.EndpointName)), diagnosticPort}
             });
-        }
-
-        private static string MakeKey(string parent, string child)
-        {
-            return FormattableString.Invariant($"{parent}:{child}");
         }
     }
 }
