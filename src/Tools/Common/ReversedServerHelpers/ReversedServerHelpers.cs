10843f49262ca409c7b62b1924f1c76a0b025fa7
David Wrighton <davidwr@microsoft.com>
2021-01-29 16:26:59 -0800

    Fix deadlock when redirecting large amounts of child process output, and add ability to not redirect child process behavior (#1945)
    * Fix deadlock when redirecting large amounts of child process output, and add ability to not redirect child process behavior
    - Use a small set of tasks to consume and ignore arbitrary data passed through standard output/error
    - Add a new switch to dotnet-trace called --redirect-child-output which can be set to false to allow viewing data. (By default the option defaults to true, which may be confusing. I'd like some feedback on that.
    
    * Revert UX changes

--- src/Tools/Common/ReversedServerHelpers/ReversedServerHelpers.cs
+++ src/Tools/Common/ReversedServerHelpers/ReversedServerHelpers.cs
@@ -19,7 +19,8 @@ namespace Microsoft.Internal.Common.Utils
     internal class ProcessLauncher
     {
         private Process _childProc = null;
-
+        private Task _stdOutTask = Task.CompletedTask;
+        private Task _stdErrTask = Task.CompletedTask;
         internal static ProcessLauncher Launcher = new ProcessLauncher();
 
         public void PrepareChildProcess(string[] args)
@@ -59,6 +60,14 @@ namespace Microsoft.Internal.Common.Utils
             return -1;
         }
 
+        private async Task ReadAndIgnoreAllStreamAsync(StreamReader streamToIgnore, CancellationToken cancelToken)
+        {
+            Memory<char> memory = new char[4096];
+            while (await streamToIgnore.ReadAsync(memory, cancelToken) != 0)
+            {
+            }
+        }
+
         public bool HasChildProc
         {
             get
@@ -74,7 +83,7 @@ namespace Microsoft.Internal.Common.Utils
                 return _childProc;
             }
         }
-        public bool Start(string diagnosticTransportName)
+        public bool Start(string diagnosticTransportName, CancellationToken ct)
         {
             _childProc.StartInfo.UseShellExecute = false;
             _childProc.StartInfo.RedirectStandardOutput = true;
@@ -91,6 +100,9 @@ namespace Microsoft.Internal.Common.Utils
                 Console.WriteLine(e.ToString());
                 return false;
             }
+            _stdOutTask = ReadAndIgnoreAllStreamAsync(_childProc.StandardOutput, ct);
+            _stdErrTask = ReadAndIgnoreAllStreamAsync(_childProc.StandardError, ct);
+
             return true;
         }
 
@@ -104,6 +116,8 @@ namespace Microsoft.Internal.Common.Utils
                 }
                 // if process exited while we were trying to kill it, it can throw IOE 
                 catch (InvalidOperationException) { }
+                _stdOutTask.Wait();
+                _stdErrTask.Wait();
             }
         }
     }
@@ -179,7 +193,7 @@ namespace Microsoft.Internal.Common.Utils
                 server.Start();
 
                 // Start the child proc
-                if (!ProcessLauncher.Launcher.Start(diagnosticTransportName))
+                if (!ProcessLauncher.Launcher.Start(diagnosticTransportName, ct))
                 {
                     throw new InvalidOperationException($"Failed to start {ProcessLauncher.Launcher.ChildProc.ProcessName}.");
                 }
