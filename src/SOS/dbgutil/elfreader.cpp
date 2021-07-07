f92d5289674eaa758d6220210829d7d3db499c37
Thefrank <1910378+Thefrank@users.noreply.github.com>
2021-07-06 21:18:36 -0700

    from feedback: use const_cast map.l_name

--- src/SOS/dbgutil/elfreader.cpp
+++ src/SOS/dbgutil/elfreader.cpp
@@ -393,20 +393,11 @@ ElfReader::EnumerateLinkMapEntries(Elf_Dyn* dynamicAddr)
             for (; i < PATH_MAX; i++)
             {
                 char ch;
-		#ifdef __FreeBSD__
-		{
 		char* l_name = const_cast<char*>(map.l_name);
 		if (!ReadMemory(l_name + i, &ch, sizeof(ch))) {
 		    TRACE("DSO: ReadMemory link_map name %p + %d FAILED\n", map.l_name, i);
                     break;
 		}
-		}
-		#else
-                if (!ReadMemory(map.l_name + i, &ch, sizeof(ch))) {
-                    TRACE("DSO: ReadMemory link_map name %p + %d FAILED\n", map.l_name, i);
-                    break;
-                }
-		#endif
                 if (ch == '\0') {
                     break;
                 }
