de51e68d2e55d47bcef9517b7cd5f5626d1a1f8f
John Salem <josalem@microsoft.com>
2021-07-19 20:01:34 +0000

    Throw unsupported exception and catch everywhere it is used.

--- src/Microsoft.Diagnostics.NETCore.Client/DiagnosticsClient/DiagnosticsClient.cs
+++ src/Microsoft.Diagnostics.NETCore.Client/DiagnosticsClient/DiagnosticsClient.cs
@@ -316,9 +316,7 @@ namespace Microsoft.Diagnostics.NETCore.Client
                     var hr = BitConverter.ToUInt32(response.Payload, 0);
                     if (hr == (uint)DiagnosticsIpcError.UnknownCommand)
                     {
-                        // We attempted to resume a 3.1 runtime, which doesn't support suspension.
-                        // noop
-                        return;
+                        throw new UnsupportedCommandException($"Resume runtime command is unknown by target runtime.");
                     }
                     throw new ServerErrorException($"Resume runtime failed (HRESULT: 0x{hr:X8})");
                 case DiagnosticsServerResponseId.OK:
