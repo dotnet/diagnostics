4740ae6abd4e91f6a6f5c6d49f066d3392797d36
Mike McLaughlin <mikem@microsoft.com>
2021-09-17 11:45:17 -0700

    Added some logging to GetLineByOffset

--- src/SOS/Strike/util.cpp
+++ src/SOS/Strike/util.cpp
@@ -5109,23 +5109,30 @@ GetLineByOffset(
     ___in ULONG cchFileName,
     ___in BOOL bAdjustOffsetForLineNumber /* = FALSE */)
 {
-    HRESULT Status = S_OK;
+    HRESULT status = S_OK;
     ULONG32 methodToken;
     ULONG32 methodOffs;
 
     // Find the image, method token and IL offset that correspond to "nativeOffset"
     ToRelease<IXCLRDataModule> pModule(NULL);
-    IfFailRet(ConvertNativeToIlOffset(nativeOffset, bAdjustOffsetForLineNumber, &pModule, &methodToken, &methodOffs));
-
-    ToRelease<IMetaDataImport> pMDImport(NULL);
-    Status = pModule->QueryInterface(IID_IMetaDataImport, (LPVOID *) &pMDImport);
-    if (FAILED(Status))
+    status = ConvertNativeToIlOffset(nativeOffset, bAdjustOffsetForLineNumber, &pModule, &methodToken, &methodOffs);
+    if (FAILED(status))
     {
-        ExtDbgOut("GetLineByOffset(%p): QueryInterface(IID_IMetaDataImport) FAILED %08x\n", nativeOffset, Status);
+        ExtDbgOut("GetLineByOffset(%p): ConvertNativeToIlOffset FAILED %08x\n", nativeOffset, status);
+        return status;
+    }
+    ToRelease<IMetaDataImport> pMDImport(NULL);
+    status = pModule->QueryInterface(IID_IMetaDataImport, (LPVOID *) &pMDImport);
+    if (FAILED(status))
+    {
+        ExtDbgOut("GetLineByOffset(%p): QueryInterface(IID_IMetaDataImport) FAILED %08x\n", nativeOffset, status);
     }
     SymbolReader symbolReader;
-    IfFailRet(symbolReader.LoadSymbols(pMDImport, pModule));
-
+    status = symbolReader.LoadSymbols(pMDImport, pModule);
+    if (FAILED(status))
+    {
+        return status;
+    }
     return symbolReader.GetLineByILOffset(methodToken, methodOffs, pLinenum, pwszFileName, cchFileName);
 }
 
