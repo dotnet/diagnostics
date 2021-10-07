9ccbd1e94c1571c4af35bda26e580474aed3a37f
Wiktor Kopec <wiktork@microsoft.com>
2021-10-07 11:00:10 -0700

    Allow asp.net duration triggers to change counter interval (#2649)
    * Allow asp.net duration triggers to change counter interval
    - Also change interval to float
    
    * PR feedback
    
    * pr feedback

--- src/Microsoft.Diagnostics.Monitoring.EventPipe/Counters/EventPipeCounterPipelineSettings.cs
+++ src/Microsoft.Diagnostics.Monitoring.EventPipe/Counters/EventPipeCounterPipelineSettings.cs
@@ -11,7 +11,10 @@ namespace Microsoft.Diagnostics.Monitoring.EventPipe
     internal class EventPipeCounterPipelineSettings : EventSourcePipelineSettings
     {
         public EventPipeCounterGroup[] CounterGroups { get; set; }
-        public TimeSpan RefreshInterval { get; set; }
+
+        //Do not use TimeSpan here since we may need to synchronize this pipeline interval
+        //with a different session and want to make sure the values are identical.
+        public float CounterIntervalSeconds { get; set; }
     }
 
     internal class EventPipeCounterGroup
--- src/tests/Microsoft.Diagnostics.Monitoring.EventPipe/EventCounterPipelineUnitTests.cs
+++ src/tests/Microsoft.Diagnostics.Monitoring.EventPipe/EventCounterPipelineUnitTests.cs
@@ -113,7 +113,7 @@ namespace Microsoft.Diagnostics.Monitoring.EventPipe.UnitTests
                             CounterNames = expectedCounters
                         }
                     },
-                    RefreshInterval = TimeSpan.FromSeconds(1)
+                    CounterIntervalSeconds = 1
                 }, new[] { logger });
 
                 await PipelineTestUtilities.ExecutePipelineWithDebugee(
