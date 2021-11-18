aa52469f7198c0f2cb9635f5a8153b08ddd00bf0
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2021-11-19 00:36:33 +0200

    Standardize on new two-line license header on non-c# files

--- src/pal/prebuilt/inc/fxverstrings.h
+++ src/pal/prebuilt/inc/fxverstrings.h
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 #ifndef VER_PRODUCTNAME_STR
         #define VER_PRODUCTNAME_STR      L"Microsoft\256 .NET Core"
