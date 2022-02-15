6b98ff435a88bd58d3893a1a7234c8f749294cef
mdh1418 <mitchhwang1418@gmail.com>
2022-02-15 17:13:15 -0500

    Remove 3rd line from header and provide AssemblyInfo context

--- src/Microsoft.Diagnostics.NETCore.Client/AssemblyInfo.cs
+++ src/Microsoft.Diagnostics.NETCore.Client/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 #if DIAGNOSTICS_RUNTIME
+// As of https://github.com/dotnet/runtime/pull/64358, InternalsVisibleTo MSBuild Item does not seem to work in Dotnet/Runtime like it does on Dotnet/Diagnostics
 using System;
 using System.IO;
 using System.Runtime.CompilerServices;
