ffbea61aa7b7ce23445e884a27b37069091b3223
Juan Sebastian Hoyos Ayala <juhoyosa@microsoft.com>
2022-03-22 20:18:49 -0700

    Adjust ProcessInfo tests to account for full commandline

--- src/tests/Microsoft.Diagnostics.NETCore.Client/CommonHelper.cs
+++ src/tests/Microsoft.Diagnostics.NETCore.Client/CommonHelper.cs
@@ -15,12 +15,8 @@ namespace Microsoft.Diagnostics.NETCore.Client
                 "..\\..\\..\\..\\..\\.dotnet\\x86\\dotnet.exe" : 
                 "..\\..\\..\\..\\..\\.dotnet\\dotnet.exe") : 
             "../../../../../.dotnet/dotnet";
-        
-        /// <summary>
-        /// gets the tracee path, with args for the dotnet host for finding the correct version of the runtime.!--
-        /// example: "--fx-version 5.0.0-rc.1.12345.12 /path/to/tracee"
-        /// </summary>
-        public static string GetTraceePathWithArgs(string traceeName = "Tracee", string targetFramework = "netcoreapp3.1")
+
+        public static string GetTraceePath(string traceeName = "Tracee", string targetFramework = "net5.0")
         {
             var curPath = Directory.GetCurrentDirectory();
 
@@ -28,13 +24,21 @@ namespace Microsoft.Diagnostics.NETCore.Client
                 .Replace(System.Reflection.Assembly.GetCallingAssembly().GetName().Name, traceeName)
                 .Replace("netcoreapp3.1", targetFramework);
 
-            traceePath = Path.Combine(traceePath, Path.ChangeExtension(traceeName, ".dll"));
+            return Path.Combine(traceePath, Path.ChangeExtension(traceeName, ".dll"));
+        }
 
+        /// <summary>
+        /// gets the tracee path, with args for the dotnet host for finding the correct version of the runtime.!--
+        /// example: "--fx-version 5.0.0-rc.1.12345.12 /path/to/tracee"
+        /// </summary>
+        public static string GetTraceePathWithArgs(string traceeName = "Tracee", string targetFramework = "net5.0")
+        {
+            string traceePath = GetTraceePath(traceeName, targetFramework);
 
             // CurrentDARCVersion is generated at build time by Microsoft.Diagnostics.NETCore.Client.UnitTests.csproj
             // This value will be set to whatever the value for the newest runtime in eng/Versions.Props is
             if (targetFramework.Equals("net5.0", StringComparison.InvariantCultureIgnoreCase))
-                traceePath = $"--fx-version {CurrentDARCVersion} {traceePath}";
+                return $"--fx-version {CurrentDARCVersion} {traceePath}";
 
             return traceePath;
         }
