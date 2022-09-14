e4d0818c71d674f2420c595c9b5c1ff120b2f3f7
Hyungju Lee <leee.lee@samsung.com>
2022-09-15 07:08:38 +0900

    Check CursorTop before changing CursorLeft to prevent ArgumentOutOfRangeException (#3372)
    * Catch ArgumentOutOfRangeException
    
    dotnet-dump analyze causes an Exception when console top is -1
    
    * Check CursorTop before changing CursorLeft

--- src/Microsoft.Diagnostics.Repl/ConsoleService.cs
+++ src/Microsoft.Diagnostics.Repl/ConsoleService.cs
@@ -267,7 +267,7 @@ namespace Microsoft.Diagnostics.Repl
 
             Console.Write(m_clearLine);
 
-            if (!m_outputRedirected) {
+            if (!m_outputRedirected && Console.CursorTop >= 0 ) {
                 Console.CursorLeft = 0;
             }
         }
@@ -313,7 +313,7 @@ namespace Microsoft.Diagnostics.Repl
 
             Console.Write("{0}{1}", prompt, text);
 
-            if (!m_outputRedirected) {
+            if (!m_outputRedirected && Console.CursorTop >= 0) {
                 Console.CursorLeft = prompt.Length + (m_cursorPosition - m_scrollPosition);
             }
         }
