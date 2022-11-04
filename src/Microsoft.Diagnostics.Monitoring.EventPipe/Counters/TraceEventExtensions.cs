72485e92442fb90b8912b224153ab297ba48d5a2
kkeirstead <kkeirstead@microsoft.com>
2022-11-04 07:03:40 -0700

    Improved metadata parsing

--- src/Microsoft.Diagnostics.Monitoring.EventPipe/Counters/TraceEventExtensions.cs
+++ src/Microsoft.Diagnostics.Monitoring.EventPipe/Counters/TraceEventExtensions.cs
@@ -23,41 +23,25 @@ namespace Microsoft.Diagnostics.Monitoring.EventPipe
                 string series = payloadFields["Series"].ToString();
                 string counterName = payloadFields["Name"].ToString();
 
-                string metadata = payloadFields["Metadata"].ToString();
-
-                // Just playing around...
-                // Read up until colon, then look for comma. If another colon shows up, fail
-                // Num_colons == num_commas - 1
-                // Also need to validate which characters are included, but that can maybe be done later...?
-                // Technically, label keys can't have colons/commas, but values can be any unicode character...
-                // without making changes in the Runtime repo, not sure we can work around this.
                 var metadataDict = new Dictionary<string, string>();
 
-                while (metadata.Length > 0)
+                string[] kvPairs = payloadFields["Metadata"].ToString().Split(",");
+
+                foreach (string kvPair in kvPairs)
                 {
-                    int colonIndex = metadata.IndexOf(':');
-                    string metadataKey = metadata.Substring(0, colonIndex);
-                    metadata = metadata.Substring(colonIndex + 1);
-                    int commaIndex = metadata.IndexOf(',');
-                    string metadataValue = string.Empty;
-                    if (commaIndex == -1)
+                    int colonIndex = kvPair.IndexOf(':');
+                    if (colonIndex == -1)
                     {
-                        if (metadata.Contains(':'))
-                        {
-                            metadataDict = new Dictionary<string, string>();
-                            break;
-                            // Fail
-                        }
-                        metadataValue = metadata;
-                        metadata = string.Empty;
+                        // Remove all entries from metadataDict if has bad formatting
+                        metadataDict.Clear();
+                        break;
                     }
                     else
                     {
-                        metadataValue = metadata.Substring(0, commaIndex);
-                        metadata = metadata.Substring(commaIndex + 1);
+                        string metadataKey = kvPair.Substring(0, colonIndex);
+                        string metadataValue = kvPair.Substring(colonIndex + 1);
+                        metadataDict[metadataKey] = metadataValue;
                     }
-
-                    metadataDict[metadataKey] = metadataValue;
                 }
 
                 //CONSIDER
