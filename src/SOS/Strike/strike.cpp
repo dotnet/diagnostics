075a71761862af26a6f27f29c8eb39d83a554fc0
Aaron Robinson <arobins@microsoft.com>
2022-11-14 19:21:08 -0800

    Bring back QI for ISOSDacInterface8

--- src/SOS/Strike/strike.cpp
+++ src/SOS/Strike/strike.cpp
@@ -1327,7 +1327,7 @@ DECLARE_API(DumpMT)
     }
 
     ReleaseHolder<ISOSDacInterface8> sos8;
-    //if (SUCCEEDED(Status = g_sos->QueryInterface(__uuidof(ISOSDacInterface8), &sos8)))
+    if (SUCCEEDED(g_sos->QueryInterface(__uuidof(ISOSDacInterface8), &sos8)))
     {
         // CLRDATA_ADDRESS assemblyLoadContext = 0;
         // Status = sos8->GetAssemblyLoadContext(TO_CDADDR(dwStartAddr), &assemblyLoadContext);
