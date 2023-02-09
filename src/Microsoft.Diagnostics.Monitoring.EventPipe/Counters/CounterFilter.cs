a2accee0e35b92b72716b13b84754fa01c76362a
wiktork <wiktork@microsoft.com>
2023-02-09 10:12:55 -0800

    Remove unnecessary using directives

--- src/Microsoft.Diagnostics.Monitoring.EventPipe/Counters/CounterFilter.cs
+++ src/Microsoft.Diagnostics.Monitoring.EventPipe/Counters/CounterFilter.cs
@@ -4,9 +4,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Reflection.Emit;
 
 namespace Microsoft.Diagnostics.Monitoring.EventPipe
 {
