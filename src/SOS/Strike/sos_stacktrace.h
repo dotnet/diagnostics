fb67ed38cbd16534907f04fa437af1a35d7e0b43
Juan Sebastian Hoyos Ayala <juan.hoyos@microsoft.com>
2023-03-01 03:52:19 -0800

    Fix license headers

--- src/SOS/Strike/sos_stacktrace.h
+++ src/SOS/Strike/sos_stacktrace.h
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 // ==++==
 //
@@ -181,4 +180,3 @@ HRESULT CALLBACK _EFN_GetManagedThread(
 #endif // __cplusplus : extern "C"
 
 #endif // __STACKTRACE_H
-
--- src/SOS/Strike/util.h
+++ src/SOS/Strike/util.h
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 #ifndef __util_h__
 #define __util_h__
