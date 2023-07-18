62f66e0c05d91438b67c5bd5fa679314793fcf54
lateralusX <lateralusx.github@gmail.com>
2023-07-18 17:15:16 +0200

    Add convert (nettrace->gcdump) command to dotnet-gcdump.

--- src/Tools/dotnet-gcdump/DotNetHeapDump/EventPipeDotNetHeapDumper.cs
+++ src/Tools/dotnet-gcdump/DotNetHeapDump/EventPipeDotNetHeapDumper.cs
@@ -21,6 +21,89 @@ namespace Microsoft.Diagnostics.Tools.GCDump
         internal static volatile bool dumpComplete;
 
         /// <summary>
+        /// Given a nettrace file from a EventPipe session with the appropriate provider and keywords turned on,
+        /// generate a GCHeapDump using the resulting events.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="memoryGraph"></param>
+        /// <param name="log"></param>
+        /// <param name="dotNetInfo"></param>
+        /// <returns></returns>
+        public static bool DumpFromEventPipeFile(string path, MemoryGraph memoryGraph, TextWriter log, DotNetHeapInfo dotNetInfo = null)
+        {
+            DateTime start = DateTime.Now;
+            Func<TimeSpan> getElapsed = () => DateTime.Now - start;
+
+            DotNetHeapDumpGraphReader dumper = new(log)
+            {
+                DotNetHeapInfo = dotNetInfo
+            };
+
+            try
+            {
+                TimeSpan lastEventPipeUpdate = getElapsed();
+
+                int gcNum = -1;
+
+                EventPipeEventSource source = new(path);
+
+                source.Clr.GCStart += delegate (GCStartTraceData data)
+                {
+                    eventPipeDataPresent = true;
+
+                    if (gcNum < 0 && data.Depth == 2 && data.Type != GCType.BackgroundGC)
+                    {
+                        gcNum = data.Count;
+                        log.WriteLine("{0,5:n1}s: .NET Dump Started...", getElapsed().TotalSeconds);
+                    }
+                };
+
+                source.Clr.GCStop += delegate (GCEndTraceData data)
+                {
+                    if (data.Count == gcNum)
+                    {
+                        log.WriteLine("{0,5:n1}s: .NET GC Complete.", getElapsed().TotalSeconds);
+                        dumpComplete = true;
+                    }
+                };
+
+                source.Clr.GCBulkNode += delegate (GCBulkNodeTraceData data)
+                {
+                    eventPipeDataPresent = true;
+
+                    if ((getElapsed() - lastEventPipeUpdate).TotalMilliseconds > 500)
+                    {
+                        log.WriteLine("{0,5:n1}s: Making GC Heap Progress...", getElapsed().TotalSeconds);
+                    }
+
+                    lastEventPipeUpdate = getElapsed();
+                };
+
+                if (memoryGraph != null)
+                {
+                    dumper.SetupCallbacks(memoryGraph, source);
+                }
+
+                log.WriteLine("{0,5:n1}s: Starting to process events", getElapsed().TotalSeconds);
+                source.Process();
+                log.WriteLine("{0,5:n1}s: Finished processing events", getElapsed().TotalSeconds);
+
+                if (eventPipeDataPresent)
+                {
+                    dumper.ConvertHeapDataToGraph();
+                }
+            }
+            catch (Exception e)
+            {
+                log.WriteLine($"{getElapsed().TotalSeconds,5:n1}s: [Error] Exception processing events: {e}");
+            }
+
+            log.WriteLine("[{0,5:n1}s: Done Dumping .NET heap success={1}]", getElapsed().TotalSeconds, dumpComplete);
+
+            return dumpComplete;
+        }
+
+        /// <summary>
         /// Given a factory for creating an EventPipe session with the appropriate provider and keywords turned on,
         /// generate a GCHeapDump using the resulting events.  The correct keywords and provider name
         /// are given as input to the Func eventPipeEventSourceFactory.
