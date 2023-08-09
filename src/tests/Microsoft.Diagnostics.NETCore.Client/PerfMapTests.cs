aeec598bcf439d347b68c25ab491c999e28e97a1
David Mason <davmason@microsoft.com>
2023-08-09 15:29:59 -0700

    Update PerfMapTests.cs
    Use one not marked aggressiveinlining

--- src/tests/Microsoft.Diagnostics.NETCore.Client/PerfMapTests.cs
+++ src/tests/Microsoft.Diagnostics.NETCore.Client/PerfMapTests.cs
@@ -88,7 +88,7 @@ namespace Microsoft.Diagnostics.NETCore.Client
 
         private void CheckWellKnownMethods(PerfMapType type, int pid)
         {
-            string[] wellKnownNames = new string[] { "Tracee.Program::Main", "System.MemoryExtensions::ContainsAny" };
+            string[] wellKnownNames = new string[] { "Tracee.Program::Main", "System.Text.Unicode.Utf16Utility::GetPointerToFirstInvalidChar" };
 
             if (type == PerfMapType.All || type == PerfMapType.PerfMap)
             {
