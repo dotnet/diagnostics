4381b4722a474e9b95b01883bcbe0a9ee51b25e9
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2023-08-27 01:29:26 +0300

    Replace alpine with linux-musl (#4189)
    Missed that during the eng/native sync, we made these replacements
    upstream in
    https://github.com/dotnet/runtime/commit/9fc5fcb0af7d068298bb1950e8d00ce26a70456c
    to keep things distro-neutral.

--- src/shared/dbgutil/elfreader.cpp
+++ src/shared/dbgutil/elfreader.cpp
@@ -306,8 +306,8 @@ ElfReader::PopulateForSymbolLookup(uint64_t baseAddress)
     // Enumerate program headers searching for the PT_DYNAMIC header, etc.
     if (!EnumerateProgramHeaders(
         baseAddress,
-#ifdef TARGET_ALPINE_LINUX
-        // On Alpine, the below dynamic entries for hash, string table, etc. are
+#ifdef TARGET_LINUX_MUSL
+        // On linux-musl, the below dynamic entries for hash, string table, etc. are
         // RVAs instead of absolute address like on all other Linux distros. Get
         // the "loadbias" (basically the base address of the module) and add to
         // these RVAs.
