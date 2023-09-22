ef523c59c84f0abe9d5bcffcc99120a404351924
kkeirstead <kkeirstead@microsoft.com>
2023-09-22 15:51:04 -0700

    Reintroduced display name for aggregate percentile payload

--- src/Microsoft.Diagnostics.Monitoring.EventPipe/Counters/CounterPayload.cs
+++ src/Microsoft.Diagnostics.Monitoring.EventPipe/Counters/CounterPayload.cs
@@ -162,6 +162,8 @@ namespace Microsoft.Diagnostics.Monitoring.EventPipe
         public AggregatePercentilePayload(string providerName, string name, string displayName, string displayUnits, string metadata, IEnumerable<Quantile> quantiles, DateTime timestamp) :
             base(timestamp, providerName, name, displayName, displayUnits, 0.0, CounterType.Metric, metadata, EventType.Histogram)
         {
+            string counterName = string.IsNullOrEmpty(displayName) ? name : displayName;
+            DisplayName = !string.IsNullOrEmpty(displayUnits) ? $"{counterName} ({displayUnits})" : counterName;
             Quantiles = quantiles.ToArray();
         }
 
