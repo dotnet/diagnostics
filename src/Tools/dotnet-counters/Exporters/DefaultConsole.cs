f84a7e54c3141118b3e8df79c7ab9102c5d81c52
Noah Falk <noahfalk@users.noreply.github.com>
2024-01-05 17:05:29 -0800

    Add dotnet-counters console exporter tests (#4376)
    Today all the testing of the console formatting is mostly manual. This
    adds some automated testing of the formatting logic in preparation to
    both refactor and make some design changes. The tests should make it
    very obvious in the future when the output formatting intentionally
    changes.

---
+++ src/Tools/dotnet-counters/Exporters/DefaultConsole.cs
@@ -0,0 +1,61 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Microsoft.Diagnostics.Tools.Counters.Exporters
+{
+    /// <summary>
+    /// The default implementation of IConsole maps everything to System.Console. In the future
+    /// maybe we could map it to System.CommandLine's IConsole, but right now that interface doesn't
+    /// have enough functionality for everything we need.
+    /// </summary>
+    internal class DefaultConsole : IConsole
+    {
+        private readonly bool _useAnsi;
+        public DefaultConsole(bool useAnsi)
+        {
+            _useAnsi = useAnsi;
+        }
+
+        public int WindowHeight => Console.WindowHeight;
+
+        public int WindowWidth => Console.WindowWidth;
+
+        // Not all platforms implement this and that is OK. Callers need to be prepared for NotSupportedException
+#pragma warning disable CA1416
+        public bool CursorVisible { get => Console.CursorVisible; set { Console.CursorVisible = value; } }
+#pragma warning restore CA1416
+
+        public int CursorTop => Console.CursorTop;
+
+        public int BufferWidth => Console.BufferWidth;
+
+        public void Clear()
+        {
+            if (_useAnsi)
+            {
+                Write($"\u001b[H\u001b[J");
+            }
+            else
+            {
+                Console.Clear();
+            }
+        }
+
+        public void SetCursorPosition(int col, int row)
+        {
+            if (_useAnsi)
+            {
+                Write($"\u001b[{row + 1};{col + 1}H");
+            }
+            else
+            {
+                Console.SetCursorPosition(col, row);
+            }
+        }
+        public void Write(string text) => Console.Write(text);
+        public void WriteLine(string text) => Console.WriteLine(text);
+        public void WriteLine() => Console.WriteLine();
+    }
+}
---
+++ src/Tools/dotnet-counters/Exporters/IConsole.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Diagnostics.Tools.Counters.Exporters
+{
+    /// <summary>
+    /// This interface abstracts the console writing code from the physical console
+    /// and allows us to do unit testing. It is similar to the IConsole interface from System.CommandLine
+    /// but unfortunately that one doesn't support all the APIs we use such as the size and positioning of
+    /// the cursor.
+    /// </summary>
+    internal interface IConsole
+    {
+        int WindowHeight { get; }
+        int WindowWidth { get; }
+        bool CursorVisible { get; set; }
+        int CursorTop { get; }
+        int BufferWidth { get; }
+
+        void Clear();
+        void SetCursorPosition(int col, int row);
+        void Write(string text);
+        void WriteLine(string text);
+        void WriteLine();
+    }
+}
