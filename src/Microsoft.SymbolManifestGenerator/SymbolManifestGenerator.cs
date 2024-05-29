a79604179ed27088cd8613e3a9b41f39800b6d3a
mdh1418 <mitchhwang1418@gmail.com>
2024-05-29 19:18:31 -0400

    [Symbol] Add manifest generator

---
+++ src/Microsoft.SymbolManifestGenerator/SymbolManifestGenerator.cs
@@ -0,0 +1,115 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.SymbolStore;
+using Microsoft.SymbolStore.KeyGenerators;
+
+namespace Microsoft.SymbolManifestGenerator
+{
+    public static class SymbolManifestGenerator
+    {
+        private const int Private = 340;
+
+        public static void GenerateManifest(ITracer tracer, DirectoryInfo dir, string manifestFileName)
+        {
+            ManifestDataV1 manifestData = new();
+
+            foreach (FileInfo file in dir.GetFiles("*", SearchOption.AllDirectories))
+            {
+                using FileStream fileStream = file.OpenRead();
+                SymbolStoreFile symbolStoreFile = new(fileStream, file.FullName);
+                FileKeyGenerator generator = new(tracer, symbolStoreFile);
+                if (!generator.IsValid())
+                {
+                    tracer.Information($"Could not generate a valid FileKeyGenerator from file '{file.FullName}'. Skipping.");
+                    continue;
+                }
+
+                foreach (SymbolStoreKey clrKey in generator.GetKeys(KeyTypeFlags.ClrKeys))
+                {
+                    string runtimeModuleDirectory = file.DirectoryName;
+                    FileInfo specialFile = GetSymbolFileToAddAdditionalDebugEntry(dir, clrKey);
+                    if (specialFile == null)
+                    {
+                        tracer.Information($"Known special file '{clrKey.FullPathName}' for runtime module '{file.FullName}' does not exist in directory '{runtimeModuleDirectory}'. Skipping.");
+                        continue;
+                    }
+
+                    string basedirRelativePath = specialFile.FullName.Replace(dir.FullName, string.Empty).TrimStart(Path.DirectorySeparatorChar);
+                    string fileHash = CalculateSHA512(specialFile);
+
+                    ManifestDataEntry manifestDataEntry = new()
+                    {
+                        BasedirRelativePath = basedirRelativePath,
+                        SymbolKey = clrKey.Index,
+                        Sha512 = fileHash,
+                        DebugInformationLevel = Private
+                    };
+
+                    manifestData.Entries.Add(manifestDataEntry);
+                }
+            }
+
+            JsonSerializerOptions serializeOptions = new()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+            string manifestDataContent = JsonSerializer.Serialize(manifestData, serializeOptions);
+            File.WriteAllText(manifestFileName, manifestDataContent);
+        }
+
+        private static FileInfo GetSymbolFileToAddAdditionalDebugEntry(DirectoryInfo basedir, SymbolStoreKey clrKey)
+        {
+            string keyFileNameToMatch = clrKey.FullPathName;
+
+            FileInfo fileOnDisk = basedir.GetFiles("*", SearchOption.AllDirectories)
+                                         .SingleOrDefault(f => f.Name.Equals(keyFileNameToMatch, StringComparison.OrdinalIgnoreCase));
+
+            if (fileOnDisk == null)
+            {
+                return null;
+            }
+
+            return fileOnDisk;
+        }
+
+        private static string CalculateSHA512(FileInfo file)
+        {
+            using FileStream fileReadStream = file.OpenRead();
+            using System.Security.Cryptography.SHA512 sha = System.Security.Cryptography.SHA512.Create();
+            byte[] hashValueBytes = sha.ComputeHash(fileReadStream);
+            return BitConverter.ToString(hashValueBytes).Replace("-", "");
+        }
+
+        private class ManifestFileVersion
+        {
+            public string Version { get; set; }
+        }
+
+        private class ManifestDataV1 : ManifestFileVersion
+        {
+            public List<ManifestDataEntry> Entries { get; set; }
+
+            public ManifestDataV1()
+            {
+                Version = "1";
+                Entries = new List<ManifestDataEntry>();
+            }
+        }
+
+        private class ManifestDataEntry
+        {
+            public string BasedirRelativePath { get; set; }
+            public string SymbolKey { get; set; }
+            public string Sha512 { get; set; }
+            public int DebugInformationLevel { get; set; }
+        }
+    }
+}
