464e06c9a8602dff44ff5c59e9a0c3777f68e41c
Juan Hoyos <19413848+hoyosjs@users.noreply.github.com>
2024-06-20 15:28:15 -0700

    Add collocation option by default for manifest generation and improve error detection and logging (#4748)
    - Change signature of `GenerateManifest` to return success or failure
    and take a parameter requesting manifest generation to consider only
    special indexes for files collocated to runtime modules.
    - Improved detection of layouts we can't handle: multiple runtimes where
    special files can't be unambiguously correlated.
    - Improved logging on error cases.

--- src/Microsoft.SymbolManifestGenerator/SymbolManifestGenerator.cs
+++ src/Microsoft.SymbolManifestGenerator/SymbolManifestGenerator.cs
@@ -10,110 +10,140 @@ using System.Text.Json.Serialization;
 using Microsoft.SymbolStore;
 using Microsoft.SymbolStore.KeyGenerators;
 
-namespace Microsoft.SymbolManifestGenerator
+namespace Microsoft.SymbolManifestGenerator;
+
+public static class SymbolManifestGenerator
 {
-    public static class SymbolManifestGenerator
+    private const int Private = 340;
+
+    public static bool GenerateManifest(ITracer tracer, DirectoryInfo dir, string manifestFileName, bool specialFilesRequireAdjacentRuntime = true)
     {
-        private const int Private = 340;
+        ManifestDataV1 manifestData = new();
 
-        public static void GenerateManifest(ITracer tracer, DirectoryInfo dir, string manifestFileName)
+        FileInfo[] allFiles = dir.GetFiles("*", SearchOption.AllDirectories);
+        foreach (FileInfo file in allFiles)
         {
-            ManifestDataV1 manifestData = new();
-
-            FileInfo[] allFiles = dir.GetFiles("*", SearchOption.AllDirectories);
-            foreach (FileInfo file in allFiles)
+            using FileStream fileStream = file.OpenRead();
+            SymbolStoreFile symbolStoreFile = new(fileStream, file.FullName);
+            FileKeyGenerator generator = new(tracer, symbolStoreFile);
+            if (!generator.IsValid())
             {
-                using FileStream fileStream = file.OpenRead();
-                SymbolStoreFile symbolStoreFile = new(fileStream, file.FullName);
-                FileKeyGenerator generator = new(tracer, symbolStoreFile);
-                if (!generator.IsValid())
+                tracer.Information($"Could not generate a valid FileKeyGenerator from file '{file.FullName}'. Skipping.");
+                continue;
+            }
+
+            // For any given runtime that's found - enumerate all possible special keys.
+            foreach (SymbolStoreKey runtimeCorrelatedKey in generator.GetKeys(KeyTypeFlags.ClrKeys))
+            {
+                (bool foundMultipleCandidates, FileInfo correlatedFile) = FindCorrelatedFileForRuntimeModule(tracer, allFiles, file, runtimeCorrelatedKey, specialFilesRequireAdjacentRuntime);
+
+                if (foundMultipleCandidates)
                 {
-                    tracer.Information($"Could not generate a valid FileKeyGenerator from file '{file.FullName}'. Skipping.");
+                    tracer.Error("Multiple files with same name to be indexed under same runtime - aborting manifest creation.");
+                    return false;
+                }
+
+                if (correlatedFile is null)
+                {
+                    tracer.Information($"Unique special file '{runtimeCorrelatedKey.FullPathName}' for runtime module '{file.FullName}' could not be found under '{file.DirectoryName}'. Skipping.");
                     continue;
                 }
 
-                foreach (SymbolStoreKey clrKey in generator.GetKeys(KeyTypeFlags.ClrKeys))
+                string basedirRelativePath = correlatedFile.FullName.Replace(dir.FullName, string.Empty).TrimStart(Path.DirectorySeparatorChar);
+                string fileHash = CalculateSHA512(correlatedFile);
+
+                if (manifestData.Entries.Any(x => x.BasedirRelativePath == basedirRelativePath))
                 {
-                    FileInfo specialFile = ResolveClrKeyToUniqueFileFromAllFiles(allFiles, clrKey);
-                    if (specialFile == null)
-                    {
-                        tracer.Information($"Known special file '{clrKey.FullPathName}' for runtime module '{file.FullName}' does not exist in directory '{file.DirectoryName}'. Skipping.");
-                        continue;
-                    }
-
-                    string basedirRelativePath = specialFile.FullName.Replace(dir.FullName, string.Empty).TrimStart(Path.DirectorySeparatorChar);
-                    string fileHash = CalculateSHA512(specialFile);
-
-                    ManifestDataEntry manifestDataEntry = new()
-                    {
-                        BasedirRelativePath = basedirRelativePath,
-                        SymbolKey = clrKey.Index,
-                        Sha512 = fileHash,
-                        DebugInformationLevel = Private,
-                        LegacyDebugInformationLevel = Private
-                    };
-
-                    manifestData.Entries.Add(manifestDataEntry);
+                    tracer.Error($"Special module '{correlatedFile.FullName}' cannot be associated with a single runtime unambiguously. Multiple runtimes found in lookup scope. Aborting manifest creation.");
+                    return false;
                 }
-            }
 
-            JsonSerializerOptions serializeOptions = new()
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                WriteIndented = true
-            };
-            string manifestDataContent = JsonSerializer.Serialize(manifestData, serializeOptions);
-            File.WriteAllText(manifestFileName, manifestDataContent);
+                ManifestDataEntry manifestDataEntry = new()
+                {
+                    BasedirRelativePath = basedirRelativePath,
+                    SymbolKey = runtimeCorrelatedKey.Index,
+                    Sha512 = fileHash,
+                    DebugInformationLevel = Private,
+                    LegacyDebugInformationLevel = Private
+                };
+
+                manifestData.Entries.Add(manifestDataEntry);
+            }
         }
+
+        JsonSerializerOptions serializeOptions = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+
+        string manifestDataContent = JsonSerializer.Serialize(manifestData, serializeOptions);
+        File.WriteAllText(manifestFileName, manifestDataContent);
+
+        return true;
 
         // Special files associated with a particular runtime module have not been guaranteed to be in the same directory as the runtime module.
-        // As such, the directory for which a manifest is being generated must guarantee that at most one file exists with the same name as the special file.
-        private static FileInfo ResolveClrKeyToUniqueFileFromAllFiles(FileInfo[] allFiles, SymbolStoreKey clrKey)
+        // in some scenarios (e.g. the ARM64 long-name binaries in the runtime pack for .NET/.NET Core).
+        // - In cases where we require the file to be collocated (CLR), just validate existence.
+        // - In cases where collocation is not guaranteed, guarantee at most one module under said name exists.
+        static (bool FoundMultipleCandidates, FileInfo File) FindCorrelatedFileForRuntimeModule(ITracer tracer, FileInfo[] allFiles, FileInfo runtimeModule, SymbolStoreKey correlatedKey, bool specialFilesRequireAdjacentRuntime)
         {
-            string clrKeyFileName = Path.GetFileName(clrKey.FullPathName);
+            string correlatedFileName = Path.GetFileName(correlatedKey.FullPathName);
 
-            FileInfo matchingSymbolFileOnDisk = allFiles.SingleOrDefault(file => FileHasClrKeyFileName(file, clrKeyFileName));
-
-            return matchingSymbolFileOnDisk;
-
-            static bool FileHasClrKeyFileName(FileInfo file, string clrKeyFileName)
+            if (specialFilesRequireAdjacentRuntime)
             {
-                return file.Name.Equals(clrKeyFileName, StringComparison.OrdinalIgnoreCase);
+                FileInfo correlatedFile = new(Path.Combine(runtimeModule.DirectoryName, correlatedFileName));
+                return (FoundMultipleCandidates: false,
+                        File: correlatedFile.Exists ? correlatedFile : default);
             }
-        }
 
-        private static string CalculateSHA512(FileInfo file)
-        {
-            using FileStream fileReadStream = file.OpenRead();
-            using System.Security.Cryptography.SHA512 sha = System.Security.Cryptography.SHA512.Create();
-            byte[] hashValueBytes = sha.ComputeHash(fileReadStream);
-            return BitConverter.ToString(hashValueBytes).Replace("-", "");
-        }
+            FileInfo[] correlatedFiles = allFiles.Where(file => file.Name.Equals(correlatedFileName, StringComparison.OrdinalIgnoreCase)).ToArray();
 
-        private class ManifestFileVersion
-        {
-            public string Version { get; set; }
-        }
-
-        private class ManifestDataV1 : ManifestFileVersion
-        {
-            public List<ManifestDataEntry> Entries { get; set; }
-
-            public ManifestDataV1()
+            if (correlatedFiles.Length > 1)
             {
-                Version = "1";
-                Entries = new List<ManifestDataEntry>();
+                tracer.Error($"Multiple files '${correlatedFileName}' found for runtime '{runtimeModule.FullName}': {string.Join<FileInfo>(", ", correlatedFiles)}.");
             }
-        }
 
-        private class ManifestDataEntry
-        {
-            public string BasedirRelativePath { get; set; }
-            public string SymbolKey { get; set; }
-            public string Sha512 { get; set; }
-            public int DebugInformationLevel { get; set; }
-            [JsonPropertyName("DebugInformationLevel")]
-            public int LegacyDebugInformationLevel { get; set; }
+            return correlatedFiles.Length switch
+            {
+                0 => (FoundMultipleCandidates: false, default),
+                1 => (FoundMultipleCandidates: false, correlatedFiles[0]),
+                _ => (FoundMultipleCandidates: true, default)
+            };
         }
+    }
+
+    private static string CalculateSHA512(FileInfo file)
+    {
+        using FileStream fileReadStream = file.OpenRead();
+        using System.Security.Cryptography.SHA512 sha = System.Security.Cryptography.SHA512.Create();
+        byte[] hashValueBytes = sha.ComputeHash(fileReadStream);
+        return BitConverter.ToString(hashValueBytes).Replace("-", "");
+    }
+
+    private class ManifestFileVersion
+    {
+        public string Version { get; set; }
+    }
+
+    private class ManifestDataV1 : ManifestFileVersion
+    {
+        public List<ManifestDataEntry> Entries { get; set; }
+
+        public ManifestDataV1()
+        {
+            Version = "1";
+            Entries = new List<ManifestDataEntry>();
+        }
+    }
+
+    private class ManifestDataEntry
+    {
+        public string BasedirRelativePath { get; set; }
+        public string SymbolKey { get; set; }
+        public string Sha512 { get; set; }
+        public int DebugInformationLevel { get; set; }
+        [JsonPropertyName("DebugInformationLevel")]
+        public int LegacyDebugInformationLevel { get; set; }
     }
 }
