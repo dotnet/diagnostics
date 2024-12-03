32ce9dc383066032a4c04f3da8215b01aeafaf0c
Mikel Blanchard <mblanchard@macrosssoftware.com>
2024-12-03 13:43:28 -0800

    Distributed tracing support.

---
+++ src/Microsoft.Diagnostics.Monitoring.EventPipe/Traces/ActivitySourceData.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+namespace Microsoft.Diagnostics.Monitoring.EventPipe
+{
+    internal sealed class ActivitySourceData
+    {
+        public ActivitySourceData(
+            string name,
+            string? version)
+        {
+            Name = name;
+            Version = version;
+        }
+
+        public string Name { get; }
+        public string? Version { get; }
+    }
+}
