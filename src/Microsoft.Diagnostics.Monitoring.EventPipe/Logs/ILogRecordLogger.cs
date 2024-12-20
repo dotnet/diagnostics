04bcfacfd75579dfa4d2046e65fccb5af62bc856
Mikel Blanchard <mblanchard@macrosssoftware.com>
2024-12-20 13:54:26 -0800

    Add a log pipeline which doesn't route through ILogger.

---
+++ src/Microsoft.Diagnostics.Monitoring.EventPipe/Logs/ILogRecordLogger.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
+using System;
+
+namespace Microsoft.Diagnostics.Monitoring.EventPipe
+{
+    internal interface ILogRecordLogger
+    {
+        void Log(
+            in LogRecord log,
+            ReadOnlySpan<KeyValuePair<string, object?>> attributes,
+            in LogRecordScopeContainer scopes);
+
+        Task PipelineStarted(CancellationToken token);
+        Task PipelineStopped(CancellationToken token);
+    }
+}
---
+++ src/Microsoft.Diagnostics.Monitoring.EventPipe/Logs/LogRecordScopeContainer.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using System.Collections.Generic;
+using System;
+
+namespace Microsoft.Diagnostics.Monitoring.EventPipe
+{
+    internal readonly ref struct LogRecordScopeContainer
+    {
+        private readonly ReadOnlySpan<LogObject> _Scopes;
+
+        public LogRecordScopeContainer(
+            ReadOnlySpan<LogObject> scopes)
+        {
+            _Scopes = scopes;
+        }
+
+        public void ForEachScope<T>(LogRecordScopeAction<T> callback, ref T state)
+        {
+            foreach (LogObject scope in _Scopes)
+            {
+                callback(scope.ToSpan(), ref state);
+            }
+        }
+
+        public delegate void LogRecordScopeAction<T>(
+            ReadOnlySpan<KeyValuePair<string, object?>> attributes,
+            ref T state);
+    }
+}
