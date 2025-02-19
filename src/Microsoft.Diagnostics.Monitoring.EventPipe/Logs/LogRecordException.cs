e6aac546fe83cb4432c98ce33d7dde17ec309965
Mikel Blanchard <mblanchard@macrosssoftware.com>
2025-02-19 13:21:51 -0800

    Code review.

--- src/Microsoft.Diagnostics.Monitoring.EventPipe/Logs/LogRecordException.cs
+++ src/Microsoft.Diagnostics.Monitoring.EventPipe/Logs/LogRecordException.cs
@@ -3,12 +3,9 @@
 
 #nullable enable
 
-using System;
-using System.Diagnostics;
-
 namespace Microsoft.Diagnostics.Monitoring.EventPipe
 {
-    internal readonly struct LogRecordException : IEquatable<LogRecordException>
+    internal readonly record struct LogRecordException
     {
         public LogRecordException(
             string? exceptionType,
@@ -25,32 +22,5 @@ namespace Microsoft.Diagnostics.Monitoring.EventPipe
         public readonly string? Message;
 
         public readonly string? StackTrace;
-
-        public bool Equals(LogRecordException other)
-        {
-            return ExceptionType == other.ExceptionType
-                && Message == other.Message
-                && StackTrace == other.StackTrace;
-        }
-
-        public override bool Equals(object obj)
-            => obj is LogRecordException ex && Equals(ex);
-
-        public override int GetHashCode()
-        {
-            HashCode hash = default;
-
-            hash.Add(ExceptionType);
-            hash.Add(Message);
-            hash.Add(StackTrace);
-
-            return hash.ToHashCode();
-        }
-
-        public static bool operator ==(LogRecordException left, LogRecordException right)
-            => left.Equals(right);
-
-        public static bool operator !=(LogRecordException left, LogRecordException right)
-            => !left.Equals(right);
     }
 }
