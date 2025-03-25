f1797a4b9c036ba603d385f0ef74c913e91468cb
pavelsavara <pavel.savara@gmail.com>
2025-03-25 16:17:29 +0100

    longer timeout for the browser WS

--- src/Microsoft.Diagnostics.NETCore.Client/ReversedServer/ReversedDiagnosticsServer.cs
+++ src/Microsoft.Diagnostics.NETCore.Client/ReversedServer/ReversedDiagnosticsServer.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Diagnostics.NETCore.Client
     {
         // The amount of time to allow parsing of the advertise data before cancelling. This allows the server to
         // remain responsive in case the advertise data is incomplete and the stream is not closed.
-        private static readonly TimeSpan ParseAdvertiseTimeout = TimeSpan.FromMilliseconds(250);
+        private static readonly TimeSpan ParseAdvertiseTimeout = TimeSpan.FromMilliseconds(750);
 
         private readonly CancellationTokenSource _disposalSource = new();
         private readonly HandleableCollection<IpcEndpointInfo> _endpointInfos = new();
