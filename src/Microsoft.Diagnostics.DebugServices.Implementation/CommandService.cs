4a663430f87e1aa40589f4befcf3c42b8b02b7e3
Adam Sitnik <adam.sitnik@gmail.com>
2025-05-28 13:41:16 +0200

    stop using System.CommandLine types that won't be public anymore

--- src/Microsoft.Diagnostics.DebugServices.Implementation/CommandService.cs
+++ src/Microsoft.Diagnostics.DebugServices.Implementation/CommandService.cs
@@ -199,7 +199,7 @@ namespace Microsoft.Diagnostics.DebugServices.Implementation
                         {
                             if (handler.IsCommandSupported(group.Parser, services))
                             {
-                                return group.GetDetailedHelp(command, services, consoleWidth);
+                                return group.GetDetailedHelp(command, services);
                             }
                             if (handler.FilterInvokeMessage != null)
                             {
@@ -276,7 +276,6 @@ namespace Microsoft.Diagnostics.DebugServices.Implementation
         {
             private Command _rootCommand;
             private readonly Dictionary<string, CommandHandler> _commandHandlers = new();
-            private readonly ParseResult _emptyParseResult;
 
             /// <summary>
             /// Create an instance of the command processor;
@@ -285,10 +284,6 @@ namespace Microsoft.Diagnostics.DebugServices.Implementation
             public CommandGroup(string commandPrompt = null)
             {
                 _rootCommand = new Command(commandPrompt);
-
-                // The actual ParseResult.Empty() has a bug in it where it tries to get the executable name
-                // and nothing is returned under lldb on Linux causing an index out of range exception.
-                _emptyParseResult = _rootCommand.Parse(Array.Empty<string>());
             }
 
             /// <summary>
@@ -317,7 +312,7 @@ namespace Microsoft.Diagnostics.DebugServices.Implementation
                     {
                         sb.AppendLine(error.Message);
                     }
-                    string helpText = GetDetailedHelp(parseResult.CommandResult.Command, services, int.MaxValue);
+                    string helpText = GetDetailedHelp(parseResult.CommandResult.Command, services);
                     throw new CommandParsingException(sb.ToString(), helpText);
                 }
                 else
@@ -433,14 +428,17 @@ namespace Microsoft.Diagnostics.DebugServices.Implementation
                 // Build or re-build parser instance after this command is added
             }
 
-            internal string GetDetailedHelp(Command command, IServiceProvider services, int windowWidth)
+            internal string GetDetailedHelp(Command command, IServiceProvider services)
             {
                 StringWriter console = new();
+                CommandLineConfiguration configuration = new(command)
+                {
+                    Output = console
+                };
 
-                // Get the command help
-                HelpBuilder helpBuilder = new(maxWidth: windowWidth);
-                HelpContext helpContext = new(helpBuilder, command, console, _emptyParseResult);
-                helpBuilder.Write(helpContext);
+                // Get the command help by parsing the --help option
+                // and invoking the help action that writes to configuration.Output.
+                command.Parse(["--help"], configuration).Invoke();
 
                 // Get the detailed help if any
                 if (TryGetCommandHandler(command.Name, out CommandHandler handler))
