56d74515e83c526fb7dec669647fb9af61477418
mdh1418 <mitchhwang1418@gmail.com>
2025-11-05 16:57:55 +0000

    [ToolsCommon] Break out LineRewriter
    LineRewriter depends on IConsole, so whenever Tools used
    CommandUtils, they would need that dependency regardless
    of whether they actually used IConsole. Like ReturnCode,
    moving LineRewriter breaks unnecessary dependencies.

--- src/Tools/Common/Commands/Utils.cs
+++ src/Tools/Common/Commands/Utils.cs
@@ -6,7 +6,6 @@ using System.Diagnostics;
 using System.Collections.Generic;
 using Microsoft.Diagnostics.NETCore.Client;
 using Microsoft.Diagnostics.Tools;
-using Microsoft.Diagnostics.Tools.Common;
 
 namespace Microsoft.Internal.Common.Utils
 {
@@ -174,73 +173,6 @@ namespace Microsoft.Internal.Common.Utils
                 }
             }
             resolvedProcessId = processId;
-        }
-    }
-
-    internal sealed class LineRewriter
-    {
-        public int LineToClear { get; set; }
-
-        private IConsole Console { get; }
-
-        public LineRewriter(IConsole console)
-        {
-            Console = console;
-        }
-
-        // ANSI escape codes:
-        //  [2K => clear current line
-        //  [{LineToClear};0H => move cursor to column 0 of row `LineToClear`
-        public void RewriteConsoleLine()
-        {
-            bool useConsoleFallback = true;
-            if (!Console.IsInputRedirected)
-            {
-                // in case of console input redirection, the control ANSI codes would appear
-
-                // first attempt ANSI Codes
-                int before = Console.CursorTop;
-                Console.Out.Write($"\u001b[2K\u001b[{LineToClear};0H");
-                int after = Console.CursorTop;
-
-                // Some consoles claim to be VT100 compliant, but don't respect
-                // all of the ANSI codes, so fallback to the System.Console impl in that case
-                useConsoleFallback = (before == after);
-            }
-
-            if (useConsoleFallback)
-            {
-                SystemConsoleLineRewriter();
-            }
-        }
-
-        private void SystemConsoleLineRewriter() => Console.SetCursorPosition(0, LineToClear);
-
-        private static bool? _isSetCursorPositionSupported;
-        public bool IsRewriteConsoleLineSupported
-        {
-            get
-            {
-                bool isSupported = _isSetCursorPositionSupported ?? EnsureInitialized();
-                return isSupported;
-
-                bool EnsureInitialized()
-                {
-                    try
-                    {
-                        int left = Console.CursorLeft;
-                        int top = Console.CursorTop;
-                        Console.SetCursorPosition(0, LineToClear);
-                        Console.SetCursorPosition(left, top);
-                        _isSetCursorPositionSupported = true;
-                    }
-                    catch
-                    {
-                        _isSetCursorPositionSupported = false;
-                    }
-                    return (bool)_isSetCursorPositionSupported;
-                }
-            }
         }
     }
 }
