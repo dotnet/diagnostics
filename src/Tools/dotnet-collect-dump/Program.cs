c326975ae42b044a2f79e233322eef7e2ee3dff2
Juan Sebastian Hoyos Ayala <juan.s.hoyos@outlook.com>
2026-01-06 03:11:08 -0800

    Add dotnet-collect-dump tool make it AOT friendly

---
+++ src/Tools/dotnet-collect-dump/Program.cs
@@ -0,0 +1,22 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.CommandLine;
+using Microsoft.Internal.Common.Commands;
+
+namespace Microsoft.Diagnostics.Tools.Dump
+{
+    internal static class Program
+    {
+        public static int Main(string[] args)
+        {
+            RootCommand rootCommand = new()
+            {
+                CollectCommandHandler.CollectCommand(),
+                ProcessStatusCommandHandler.ProcessStatusCommand("Lists the dotnet processes that dumps can be collected from.")
+            };
+
+            return rootCommand.Parse(args).Invoke();
+        }
+    }
+}
