a146124a4bdda89051b6fa7e9b7677805ec391a2
Juan Hoyos <19413848+hoyosjs@users.noreply.github.com>
2026-05-11 10:11:47 -0700

    Apply suggestions from code review
    Co-authored-by: Juan Hoyos <19413848+hoyosjs@users.noreply.github.com>

--- src/tests/CommonTestRunner/Debuggees/EventPipeTracee/Program.cs
+++ src/tests/CommonTestRunner/Debuggees/EventPipeTracee/Program.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO.Pipes;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
