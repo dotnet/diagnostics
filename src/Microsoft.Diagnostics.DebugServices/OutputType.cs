b9f6e148735ca5b235dcd7051842fd9ba3145d58
Mike McLaughlin <mikem8361@hotmail.com>
2026-08-02 11:14:30 -0700

    Improve and simplify the IConsoleService interface (#5924)
    Combined all the explicit Write functions into one function with the
    output type.
    
    First step in improving the overall (managed and native) console
    infrastructure.

---
+++ src/Microsoft.Diagnostics.DebugServices/OutputType.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.Diagnostics.DebugServices
+{
+    public enum OutputType
+    {
+        Default = 0,
+        Logging = 1,    // Used when logging to console is enabled. Allows the command output capture to ignore SOS logging output.
+        Dml = 2,        // throws NotSupportedException if DML isn't supported or enabled.
+    };
+}
