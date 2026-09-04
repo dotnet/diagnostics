1b5667c92398247eebc8c25fb8d684a631c1e4ee
Steve Pfister <steveisok@users.noreply.github.com>
2026-09-03 21:34:30 -0400

    Revert "Add new SOS test harness" (#6006)
    Reverts dotnet/diagnostics#5979

--- src/tests/SOS.TestHarness.Capturer/Program.cs
+++
@@ -1,8 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using SOS.TestHarness;
-
-// Out-of-process desktop-dump capturer. Runs the risky in-process dbgeng live-debugging work in
-// a short-lived child process so it can never crash the test host. See CaptureCli for usage.
-return CaptureCli.Run(args);
